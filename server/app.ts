a280c137680388e9490e1fedc5f9db10df85f482
RobertTheDev <robertthedev@gmail.com>
2024-01-06 23:53:38 +0000

    feat: installed and set up cors

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
 app.use(express.json());
+
+app.use(cors());
 
 const serverPort = process.env.PORT;
 
