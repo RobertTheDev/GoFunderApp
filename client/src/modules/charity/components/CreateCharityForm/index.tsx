9bd7ab7df5d55a86ccde73b5f041f1af04b49798
RobertTheDev <robertthedev@gmail.com>
2024-01-07 03:14:06 +0000

    feat: created and added create charity form

---
+++ client/src/modules/charity/components/CreateCharityForm/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function CreateCharityForm(): ReactElement {
+  return (
+    <div>
+      <p>Create Charity Form</p>
+    </div>
+  );
+}
--- client/src/modules/charity/routes/CreateCharity/index.tsx
+++ client/src/modules/charity/routes/CreateCharity/index.tsx
@@ -1,9 +1,11 @@
 import { ReactElement } from "react";
+import CreateCharityForm from "../../components/CreateCharityForm";
 
 export default function CreateCharityRoute(): ReactElement {
   return (
     <div>
       <p>Create Charity</p>
+      <CreateCharityForm />
     </div>
   );
 }
