ee3e9a7656d6ec6cde4a0e648ae6ad42bf637613
RobertTheDev <robertthedev@gmail.com>
2024-01-07 03:13:29 +0000

    feat: added create charity link

--- client/src/modules/layout/components/Header/index.tsx
+++ client/src/modules/layout/components/Header/index.tsx
@@ -5,6 +5,7 @@ export default function Header(): ReactElement {
   return (
     <header>
       <Link to={"/"}>Home</Link>
+      <Link to={"/charities/create-charity"}>Create Charity</Link>
     </header>
   );
 }
