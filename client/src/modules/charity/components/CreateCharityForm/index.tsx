3659fe8984379313c192dc317107c6c8b91c9a88
RobertTheDev <robertthedev@gmail.com>
2024-01-07 03:40:13 +0000

    feat: created create charity form

--- client/src/modules/charity/components/CreateCharityForm/index.tsx
+++ client/src/modules/charity/components/CreateCharityForm/index.tsx
@@ -1,9 +1,78 @@
-import { ReactElement } from "react";
+import axios from "axios";
+import { ReactElement, useState } from "react";
 
 export default function CreateCharityForm(): ReactElement {
+  const [charityForm, setCharityForm] = useState({
+    category: "",
+    name: "",
+    logoUrl: "",
+    description: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setCharityForm({
+      ...charityForm,
+      [name]: value,
+    });
+  };
+
+  async function createCharity() {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/charities/create`,
+        charityForm
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createCharity();
+  };
+
   return (
-    <div>
-      <p>Create Charity Form</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        value={charityForm.name}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor="description">Description</label>
+      <input
+        type="text"
+        name="description"
+        value={charityForm.description}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor="logoUrl">Logo URL</label>
+      <input
+        type="url"
+        name="logoUrl"
+        value={charityForm.logoUrl}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="category">Category</label>
+      <input
+        type="text"
+        name="category"
+        value={charityForm.category}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Create Charity</button>
+    </form>
   );
 }
