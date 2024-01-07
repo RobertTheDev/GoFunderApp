15c775b3c737b8bbe588daff6fc769f943f25b1e
RobertTheDev <robertthedev@gmail.com>
2024-01-07 01:34:40 +0000

    feat: set up the start server handler

--- server/app.ts
+++ server/app.ts
@@ -1,37 +1,4 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import startServer from "./src/configs/startServer";
 
-// Set up express.
-const app = express();
-app.use(express.json());
-
-// Set up cors.
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "DELETE", "POST", "PUT"],
-  })
-);
-
-// Get the server port from dotenv.
-const serverPort = process.env.PORT;
-
-const prismaClient = new PrismaClient();
-
-// Get the charities data from route.
-app.get("/api/charities", async (_req, res) => {
-  try {
-    const charities = await prismaClient.charity.findMany();
-
-    res.json(charities);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-// Run the server port.
-app.listen(serverPort, () => {
-  console.log(`App is running on ${serverPort}.`);
-});
+// Runs the express server.
+startServer();
