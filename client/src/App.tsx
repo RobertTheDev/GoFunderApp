457c355bd0cc898a8da813ea0a4c4b07c7f2b0dd
RobertTheDev <robertthedev@gmail.com>
2024-01-07 00:29:26 +0000

    feat: fetched charities data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [charities, setCharities] = useState<{ name: string }[]>([]);
+
+  async function getCharities() {
+    try {
+      const charities = await axios.get(
+        `${process.env.REACT_APP_API_URL}/charities`
+      );
+
+      setCharities(charities.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getCharities();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{JSON.stringify(charities)}</p>
     </div>
   );
 }
