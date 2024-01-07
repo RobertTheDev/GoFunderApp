ebc735e6c9fc1a74321a71f0f8d314218d22bec5
RobertTheDev <robertthedev@gmail.com>
2024-01-07 00:00:30 +0000

    feat: set up chairties route

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,22 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+// Set up express.
 const app = express();
 app.use(express.json());
 
+// Set up cors.
 app.use(cors());
 
+// Get the server port from dotenv.
 const serverPort = process.env.PORT;
 
+// Get the charities data from route.
+app.get("/charities", (_req, res) => {
+  res.json([{ name: "Save Africa" }, { name: "Help the children" }]);
+});
+
+// Run the server port.
 app.listen(serverPort, () => {
   console.log(`App is running on ${serverPort}.`);
 });
