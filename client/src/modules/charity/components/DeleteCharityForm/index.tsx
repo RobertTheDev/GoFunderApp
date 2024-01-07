61ec66c6ad15b0690a6bd9435996b9ef1b8dbfe5
RobertTheDev <robertthedev@gmail.com>
2024-01-07 03:52:03 +0000

    feat: created delete charity form

--- client/src/modules/charity/components/DeleteCharityForm/index.tsx
+++ client/src/modules/charity/components/DeleteCharityForm/index.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import { ReactElement } from "react";
+import { useParams } from "react-router-dom";
 
 export default function DeleteCharityForm(): ReactElement {
+  const { id } = useParams();
+
+  async function handleDeleteCharity() {
+    await axios.delete(
+      `${process.env.REACT_APP_API_URL}/api/charities/${id}/delete`
+    );
+  }
+
   return (
     <div>
       <p>Delete Charity Form</p>
+      <button type="button" onClick={handleDeleteCharity}>
+        Delete
+      </button>
     </div>
   );
 }
