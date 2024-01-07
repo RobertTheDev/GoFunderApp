7d8cc532aa9ed0cf4710b65234541b44de2fbfad
RobertTheDev <robertthedev@gmail.com>
2024-01-07 04:08:26 +0000

    added http status codes

--- server/src/modules/charity/charity.controllers.ts
+++ server/src/modules/charity/charity.controllers.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../utils/prisma/prismaClient";
 import { Request, Response } from "express";
+import { ReasonPhrases, StatusCodes, getReasonPhrase } from "http-status-codes";
 
 export async function createCharity(req: Request, res: Response) {
   const { body } = req;
@@ -9,9 +10,13 @@ export async function createCharity(req: Request, res: Response) {
       data: body,
     });
 
-    res.json(charity);
+    res.status(StatusCodes.CREATED).json(charity);
   } catch (error) {
     console.error(error);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
   }
 }
 
@@ -20,9 +25,17 @@ export async function getCharities(_req: Request, res: Response) {
   try {
     const charities = await prismaClient.charity.findMany();
 
-    res.json(charities);
+    res.status(StatusCodes.OK).json({
+      reason: ReasonPhrases.OK,
+      message: "Successfully found charities.",
+      data: charities,
+    });
   } catch (error) {
     console.error(error);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
   }
 }
 
@@ -36,9 +49,24 @@ export async function getCharityById(req: Request, res: Response) {
       where: { id },
     });
 
-    res.json(charity);
+    if (!charity) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        reason: ReasonPhrases.NOT_FOUND,
+        message: "Could not find charity with that id",
+        data: null,
+      });
+    }
+    res.status(StatusCodes.OK).json({
+      reason: ReasonPhrases.OK,
+      message: "Successfully found charity.",
+      data: charity,
+    });
   } catch (error) {
     console.error(error);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
   }
 }
 
@@ -54,9 +82,17 @@ export async function updateCharityById(req: Request, res: Response) {
       where: { id },
     });
 
-    res.json(updatedCharity);
+    res.status(StatusCodes.OK).json({
+      reason: ReasonPhrases.OK,
+      message: "Successfully updated charity.",
+      data: updatedCharity,
+    });
   } catch (error) {
     console.error(error);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
   }
 }
 
@@ -70,8 +106,16 @@ export async function deleteCharityById(req: Request, res: Response) {
       where: { id },
     });
 
-    res.json({});
+    res.status(StatusCodes.OK).json({
+      reason: ReasonPhrases.OK,
+      message: "Successfully deleted charity.",
+      data: null,
+    });
   } catch (error) {
     console.error(error);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
   }
 }
