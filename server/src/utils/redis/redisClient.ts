10fbf7501974953b98f9dce0d984ece40033c198
RobertTheDev <robertthedev@gmail.com>
2024-01-07 04:39:50 +0000

    feat: created redis client

---
+++ server/src/utils/redis/redisClient.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+/* Pulls the Redis URL from .env */
+const redisUrl = process.env.REDIS_URL;
+
+/* Create and open the Redis Client */
+const redisClient = createClient({ url: redisUrl });
+
+redisClient.on("error", (err) => {
+  console.error(`Redis client error: ${err}`);
+});
+
+redisClient.on("end", () => {
+  console.log("Redis client connection closed");
+});
+
+redisClient.connect();
+
+export default redisClient;
