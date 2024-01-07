47c64495df2799c33ea5b85b389445f81e7d26e2
RobertTheDev <robertthedev@gmail.com>
2024-01-07 03:40:03 +0000

    feat: created create charity controller

--- server/src/modules/charity/charity.controllers.ts
+++ server/src/modules/charity/charity.controllers.ts
@@ -1,6 +1,20 @@
 import prismaClient from "../../utils/prisma/prismaClient";
 import { Request, Response } from "express";
 
+export async function createCharity(req: Request, res: Response) {
+  const { body } = req;
+
+  try {
+    const charity = await prismaClient.charity.create({
+      data: body,
+    });
+
+    res.json(charity);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 // Gets all charities from the prisma database.
 export async function getCharities(_req: Request, res: Response) {
   try {
