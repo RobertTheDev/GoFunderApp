2061c6e46aa44dd2043d47d823739c151320dbb9
RobertTheDev <robertthedev@gmail.com>
2024-01-08 22:38:28 +0000

    feat: defined start server handler

--- server/src/configs/startServer/index.ts
+++ server/src/configs/startServer/index.ts
@@ -7,42 +7,48 @@ import helmet from 'helmet'
 import compression from 'compression'
 import winstonLogger from 'src/utils/winston/winstonLogger.js'
 import cookieParser from 'cookie-parser'
-import rateLimiter from 'src/utils/limiter/rateLimiter.js'
+// import rateLimiter from 'src/utils/limiter/rateLimiter.js'
 
 // This handler runs the express server when called.
 
+// Set up express.
+const app = express()
+app.use(express.json())
+
+// Set up helmet.
+app.use(helmet())
+
+// Set up compression.
+app.use(compression())
+
+// Set up cookie parser.
+app.use(cookieParser())
+
+// Set up cors.
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'DELETE', 'POST', 'PUT'],
+  }),
+)
+
+// Sets up rate limiting.
+// app.use(rateLimiter)
+
+// Get the server port from dotenv.
+const serverPort = process.env.SERVER_PORT
+
+// Sets up the app router.
+app.use('/api', appRouter as RequestHandler)
+
+// Run the server port.
 export default function startServer(): void {
-  // Set up express.
-  const app = express()
-  app.use(express.json())
-
-  // Set up helmet.
-  app.use(helmet())
-
-  // Set up compression.
-  app.use(compression())
-
-  // Set up cookie parser.
-  app.use(cookieParser())
-
-  // Set up cors.
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'DELETE', 'POST', 'PUT'],
-    }),
-  )
-
-  app.use(rateLimiter)
-
-  // Get the server port from dotenv.
-  const serverPort = process.env.SERVER_PORT
-
-  // Sets up the app router.
-  app.use('/api', appRouter as RequestHandler)
-
-  // Run the server port.
-  app.listen(serverPort, () => {
-    winstonLogger.info(`App is running on ${serverPort}.`)
-  })
+  try {
+    app.listen(serverPort, () => {
+      winstonLogger.info(`App is running on ${serverPort}.`)
+    })
+  } catch (error) {
+    winstonLogger.error(error)
+    process.exit(1)
+  }
 }
