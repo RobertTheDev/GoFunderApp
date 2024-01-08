e2e3dd11a7964e6e697d6b5b021f91df577ddba9
RobertTheDev <robertthedev@gmail.com>
2024-01-08 22:37:44 +0000

    feat: created cache service

---
+++ server/src/services/cache/cache.service.ts
@@ -0,0 +1,23 @@
+import redisClient from 'src/utils/redis/redisClient'
+
+export class CacheService {
+  private readonly cache = redisClient
+
+  async get(key: string): Promise<string | null> {
+    return await this.cache.get(key)
+  }
+
+  async set(params: {
+    key: string
+    value: any
+    expiry: number
+  }): Promise<string | null> {
+    const { key, value, expiry } = params
+
+    return await this.cache.set(key, JSON.stringify(value), { EX: expiry })
+  }
+
+  async delete(key: string): Promise<number> {
+    return await this.cache.del(key)
+  }
+}
