30e19bcf0782b80ebbea5752f9b03c6261b71439
RobertTheDev <robertthedev@gmail.com>
2024-01-09 05:09:58 +0000

    feat: added auth routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import charityRouter from '../modules/charity/charity.routes.js'
+import authRouter from 'src/modules/auth/auth.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const appRouter = Router()
 
 // Defines app routes.
+appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
 
 export default appRouter
