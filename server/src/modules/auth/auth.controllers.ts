e140786302500f9ea663932a6884187efc01f705
RobertTheDev <robertthedev@gmail.com>
2024-01-09 05:42:00 +0000

    feat: added password hashng logic

--- server/src/modules/auth/auth.controllers.ts
+++ server/src/modules/auth/auth.controllers.ts
@@ -1,10 +1,13 @@
 import type { Request, Response } from 'express'
 import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import { hashPassword } from 'src/configs/passwordManagement'
 import winstonLogger from 'src/utils/winston/winstonLogger'
 
 export async function signUp(_req: Request, res: Response): Promise<void> {
   try {
-    res.send('hello')
+    const password = await hashPassword('password')
+
+    res.send({ password })
   } catch (error) {
     winstonLogger.error(error)
 
