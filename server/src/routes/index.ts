e55846e9cc6bd6e2e1abc4b615e0c75ca0b4666c
RobertTheDev <robertthedev@gmail.com>
2024-01-09 18:42:06 +0000

    feat: created send email route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import charityRouter from '../modules/charity/charity.routes.js'
-import authRouter from 'src/modules/auth/auth.routes.js'
+import authRouter from '../modules/auth/auth.routes.js'
+import emailRouter from '../modules/email/email.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const appRouter = Router()
@@ -8,5 +9,6 @@ const appRouter = Router()
 // Defines app routes.
 appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
+appRouter.use('/email', emailRouter)
 
 export default appRouter
