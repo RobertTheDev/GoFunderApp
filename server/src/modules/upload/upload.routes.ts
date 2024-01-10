e8e405504c64c8a74c98918bdc2983be507367e4
RobertTheDev <robertthedev@gmail.com>
2024-01-10 00:36:14 +0000

    feat: defined upload routes

---
+++ server/src/modules/upload/upload.routes.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from 'express'
+import { Router } from 'express'
+import { uploadImage } from './upload.controllers'
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (_req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+const upload = multer({ storage })
+
+// const upload = multer({ dest: 'uploads/' })
+
+// Sets up the upload router.
+const uploadRouter = Router()
+
+// Defines the upload routes.
+uploadRouter.post(
+  '/upload-image',
+  upload.single('avatar'),
+  uploadImage as RequestHandler,
+)
+
+export default uploadRouter
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import charityRouter from '../modules/charity/charity.routes.js'
 import authRouter from '../modules/auth/auth.routes.js'
 import emailRouter from '../modules/email/email.routes.js'
+import uploadRouter from '../modules/upload/upload.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const appRouter = Router()
@@ -10,5 +11,6 @@ const appRouter = Router()
 appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
 appRouter.use('/email', emailRouter)
+appRouter.use('/uploads', uploadRouter)
 
 export default appRouter
