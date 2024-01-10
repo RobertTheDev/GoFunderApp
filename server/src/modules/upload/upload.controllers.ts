c91c3e398dba8bf662dc945644775a4bfd75e3ad
RobertTheDev <robertthedev@gmail.com>
2024-01-10 00:36:21 +0000

    feat: defined upload controller

---
+++ server/src/modules/upload/upload.controllers.ts
@@ -0,0 +1,17 @@
+import type { Request, Response } from 'express'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import winstonLogger from '../../utils/winston/winstonLogger'
+
+export async function uploadImage(req: Request, res: Response): Promise<void> {
+  try {
+    winstonLogger.info(JSON.stringify(req.file))
+
+    res.send({ message: 'Image uploaded' })
+  } catch (error) {
+    winstonLogger.error(error)
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: ReasonPhrases.INTERNAL_SERVER_ERROR,
+    })
+  }
+}
