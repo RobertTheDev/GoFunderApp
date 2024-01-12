b85786ba16d32d1eac4de5b80685732d376d99c9
RobertTheDev <robertthedev@gmail.com>
2024-01-12 04:34:14 +0000

    feat: defined auth routes

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,12 +1,17 @@
 import type { RequestHandler } from 'express'
 import { Router } from 'express'
-import { getAuthenticatedUser, signUp } from './auth.controllers'
+import { getAuthenticatedUser, signOut } from './auth.controllers'
+import emailPasswordRouter from './emailPassword/emailPassword.routes'
+import magicLinkRouter from './magicLink/magicLink.routes'
 
 // Sets up the charity router.
 const authRouter = Router()
 
 // Defines the charity routes.
 authRouter.get('/user', getAuthenticatedUser as RequestHandler)
-authRouter.post('/sign-up', signUp as RequestHandler)
+authRouter.post('/sign-out', signOut as RequestHandler)
+
+authRouter.use('/', emailPasswordRouter)
+authRouter.use('/magic-link', magicLinkRouter)
 
 export default authRouter
