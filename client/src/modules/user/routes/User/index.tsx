5bb8bfb0286feba14d381b2b8f33f97ae6790e57
RobertTheDev <robertthedev@gmail.com>
2024-01-13 02:47:18 +0000

    feat: created user route

---
+++ client/src/modules/user/routes/User/index.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Seo from "../../../seo/components/Seo";
+import { useQuery } from "@tanstack/react-query";
+import { ReactElement } from "react";
+import IUser from "../../../../interfaces/User";
+
+export default function UserRoute(): ReactElement {
+  const { id } = useParams();
+
+  const { isPending, error, data } = useQuery<IUser | null>({
+    queryKey: ["getUserData"],
+    queryFn: async () =>
+      await axios
+        .get(`${process.env.REACT_APP_API_URL}/api/users/${id}`)
+        .then((res) => res.data.data),
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: + {error.message}</p>;
+
+  if (!data) return <p>No user found.</p>;
+
+  return (
+    <div>
+      <Seo title={`${data.firstName} ${data.lastName}`} />
+
+      <p>
+        {data.firstName} {data.lastName}
+      </p>
+
+      <img
+        style={{
+          height: 300,
+          width: 300,
+        }}
+        src={data.image ? data.image : ""}
+        alt={"Profile avatar"}
+      />
+    </div>
+  );
+}
