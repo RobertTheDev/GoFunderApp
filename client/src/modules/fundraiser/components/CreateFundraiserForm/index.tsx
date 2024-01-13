6c3e94ffccbc30befc95ee6d731919de318ac254
RobertTheDev <robertthedev@gmail.com>
2024-01-13 10:08:30 +0000

    feat: defined fundraiser components

---
+++ client/src/modules/fundraiser/components/CreateFundraiserForm/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function CreateFundraiserForm(): ReactElement {
+  return (
+    <div>
+      <p>Create Fundraiser Form</p>
+    </div>
+  );
+}
---
+++ client/src/modules/fundraiser/components/FundraiserCard/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function FundraiserCard(): ReactElement {
+  return (
+    <div>
+      <p>Fundraiser Card</p>
+    </div>
+  );
+}
