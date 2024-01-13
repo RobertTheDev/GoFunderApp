4175c6adb27728e46df3bfbb49000170c95288b6
RobertTheDev <robertthedev@gmail.com>
2024-01-13 00:54:53 +0000

    feat: created saved fundraiser validators

---
+++ server/src/modules/savedFundraiser/savedFundraiser.validators.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+export const createSavedFundraiserSchema = z.object({
+  fundraiserId: z
+    .string({
+      invalid_type_error: 'Fundraiser ID must be a string.',
+      required_error: 'Fundraiser ID is required.',
+    })
+    .min(1, 'Fundraiser ID cannot be empty.'),
+  userId: z
+    .string({
+      invalid_type_error: 'User ID must be a string.',
+      required_error: 'User ID is required.',
+    })
+    .min(1, 'User ID cannot be empty.'),
+})
