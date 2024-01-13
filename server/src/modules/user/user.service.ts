226cf0f9679a05908a2c9d9099a425253562bf8e
RobertTheDev <robertthedev@gmail.com>
2024-01-13 02:47:28 +0000

    feat: added find user to service

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -4,6 +4,14 @@ import { PrismaClient } from '@prisma/client'
 export class UserService {
   private readonly prisma = new PrismaClient()
 
+  async findUser(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    })
+  }
+
   async findUsers(params: {
     skip?: number
     take?: number
