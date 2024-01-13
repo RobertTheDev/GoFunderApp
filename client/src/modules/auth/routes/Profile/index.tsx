6d6c5ac2c1469cdad6e56175b43ed872824ca61b
RobertTheDev <robertthedev@gmail.com>
2024-01-13 10:03:35 +0000

    feat: added logout handler

--- client/src/modules/auth/routes/Profile/index.tsx
+++ client/src/modules/auth/routes/Profile/index.tsx
@@ -1,39 +1,71 @@
 import axios from "axios";
 import Seo from "../../../seo/components/Seo";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ReactElement } from "react";
 import IUser from "../../../../interfaces/User";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 export default function ProfileRoute(): ReactElement {
+  const navigate: NavigateFunction = useNavigate();
+
+  async function signOut() {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/auth/sign-out`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+
+      navigate("/");
+    } catch (error: any) {
+      console.error("Sign-out error:", error);
+    }
+  }
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["getProfileData"] });
+    },
+    onError: () => {
+      console.log("error");
+    },
+  });
+
   const { isPending, error, data } = useQuery<IUser | null>({
     queryKey: ["getProfileData"],
-    queryFn: async () =>
-      await axios
-        .get(`${process.env.REACT_APP_API_URL}/api/auth/user`, {
+    queryFn: async () => {
+      const user = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/auth/user`,
+        {
           withCredentials: true,
-        })
-        .then((res) => res.data.user),
+        }
+      );
+
+      return user.data.data;
+    },
   });
 
   if (isPending) return <p>Loading...</p>;
 
-  if (error) return <p>An error has occurred: + {error.message}</p>;
+  if (error) navigate("/");
 
-  if (!data) return <p>No user found.</p>;
-
-  async function signOut() {
-    await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/sign-out`, {
-      withCredentials: true,
-    });
-  }
+  if (!data) navigate("/");
 
   return (
     <div>
       <Seo title={"Profile"} />
 
-      <p>{data.email}</p>
+      <p>{data?.email}</p>
 
-      <button onClick={signOut}>Sign Out</button>
+      <img src={data?.image ? data.image : ""} alt="Avatart" />
+
+      <button onClick={() => mutation.mutate()}>Sign Out</button>
     </div>
   );
 }
