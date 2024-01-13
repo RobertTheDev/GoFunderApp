24d14b031036d87a3386186d2b055bc089901b3d
RobertTheDev <robertthedev@gmail.com>
2024-01-13 01:54:47 +0000

    feat: defined donation routes

---
+++ server/src/modules/donation/donation.routes.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from 'express'
+import { Router } from 'express'
+import {
+  createDonation,
+  getDonationById,
+  getDonations,
+  getDonationsByCurrentUser,
+  getDonationsByFundraiserId,
+  getDonationsByUserId,
+} from './donation.controllers'
+import { ensureUserIsAuthenticated } from '../auth/auth.middlewares'
+
+// Sets up the donation router.
+const donationRouter = Router()
+
+// Defines the donation routes.
+donationRouter.get('/', getDonations as RequestHandler)
+donationRouter.get('/:id', getDonationById as RequestHandler)
+donationRouter.get(
+  '/fundraiser/:id',
+  getDonationsByFundraiserId as RequestHandler,
+)
+donationRouter.get('/user/:id', getDonationsByUserId as RequestHandler)
+donationRouter.get(
+  '/current-user',
+  ensureUserIsAuthenticated as RequestHandler,
+  getDonationsByCurrentUser as RequestHandler,
+)
+donationRouter.post('/create', createDonation as RequestHandler)
+
+export default donationRouter
