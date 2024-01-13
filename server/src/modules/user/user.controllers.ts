8e592bb7f70e136e8ce80474b002500ebf922ec4
RobertTheDev <robertthedev@gmail.com>
2024-01-13 02:47:43 +0000

    feat: defined get user by id

--- server/src/modules/user/user.controllers.ts
+++ server/src/modules/user/user.controllers.ts
@@ -26,3 +26,39 @@ export async function getAllUsers(
     })
   }
 }
+
+// Gets a user by its id from the prisma database.
+export async function getUserById(
+  req: Request,
+  res: Response,
+): Promise<Response<any>> {
+  const {
+    params: { id },
+  } = req
+
+  try {
+    const user = await userService.findUser({
+      id,
+    })
+
+    if (user === null) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        status: ReasonPhrases.NOT_FOUND,
+        message: 'Could not find user with that id.',
+        data: null,
+      })
+    }
+
+    return res.status(StatusCodes.OK).json({
+      status: ReasonPhrases.OK,
+      message: 'Successfully found user.',
+      data: user,
+    })
+  } catch (error) {
+    winstonLogger.error(error)
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    })
+  }
+}
