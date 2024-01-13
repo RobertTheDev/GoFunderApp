f3bc3e1629fc48271cf8da462a653fbc4b751cae
RobertTheDev <robertthedev@gmail.com>
2024-01-13 01:54:55 +0000

    feat: defined donation services

---
+++ server/src/modules/donation/donation.service.ts
@@ -0,0 +1,48 @@
+import type { Donation, Prisma } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+
+export class DonationService {
+  private readonly prisma = new PrismaClient()
+
+  async findDonations(params: {
+    skip?: number
+    take?: number
+    cursor?: Prisma.DonationWhereUniqueInput
+    where?: Prisma.DonationWhereInput
+    orderBy?: Prisma.DonationOrderByWithRelationInput
+  }): Promise<Donation[]> {
+    const { skip, take, cursor, where, orderBy } = params
+    return this.prisma.donation.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    })
+  }
+
+  async createDonation(data: Prisma.DonationCreateInput): Promise<Donation> {
+    return this.prisma.donation.create({
+      data,
+    })
+  }
+
+  async updateDonation(params: {
+    where: Prisma.DonationWhereUniqueInput
+    data: Prisma.DonationUpdateInput
+  }): Promise<Donation> {
+    const { data, where } = params
+    return this.prisma.donation.update({
+      data,
+      where,
+    })
+  }
+
+  async deleteDonation(
+    where: Prisma.DonationWhereUniqueInput,
+  ): Promise<Donation> {
+    return this.prisma.donation.delete({
+      where,
+    })
+  }
+}
