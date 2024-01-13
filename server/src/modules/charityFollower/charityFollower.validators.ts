24c213cc6b7b9720edd0576f00f19080353b185b
RobertTheDev <robertthedev@gmail.com>
2024-01-13 00:53:52 +0000

    feat: created charity follower validators

---
+++ server/src/modules/charityFollower/charityFollower.validators.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+export const createCharityFollowerSchema = z.object({
+  charityId: z
+    .string({
+      invalid_type_error: 'Charity ID must be of type string.',
+      required_error: 'Charity ID is required.',
+    })
+    .min(1, 'Charity ID cannot be empty.'),
+  userId: z
+    .string({
+      invalid_type_error: 'User ID must be of type string.',
+      required_error: 'User ID is required.',
+    })
+    .min(1, 'User ID cannot be empty.'),
+})
