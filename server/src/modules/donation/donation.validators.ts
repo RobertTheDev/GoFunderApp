37bd5309fcbd31a4336f6b66754686a6ad9b8132
RobertTheDev <robertthedev@gmail.com>
2024-01-13 00:54:04 +0000

    feat: created donation validators

---
+++ server/src/modules/donation/donation.validators.ts
@@ -0,0 +1,50 @@
+import z from 'zod'
+
+export const createDonationSchema = z.object({
+  amount: z
+    .number({
+      invalid_type_error: 'Amount must be of type integer.',
+      required_error: 'Amount is required.',
+    })
+    .min(1, 'Amount cannot be empty.'),
+  annonymous: z
+    .boolean({
+      invalid_type_error: 'Annonymous must be of type boolean.',
+    })
+    .optional(),
+  fundraiserId: z
+    .string({
+      invalid_type_error: 'Fundraiser ID must be of type string.',
+      required_error: 'Fundraiser ID is required',
+    })
+    .min(1, 'Fundraiser ID cannot be empty.'),
+  message: z
+    .string({
+      invalid_type_error: 'Message must be of type string.',
+      required_error: 'Message is required.',
+    })
+    .min(1, 'Message cannot be empty.'),
+})
+
+// export const updateDonationSchema = z.object({
+//   amount: z
+//     .number({
+//       invalid_type_error: 'Amount must be of type integer.',
+//     })
+//     .min(1, 'Amount cannot be empty.'),
+//   annonymous: z
+//     .boolean({
+//       invalid_type_error: 'Annonymous must be of type boolean.',
+//     })
+//     .optional(),
+//   fundraiserId: z
+//     .string({
+//       invalid_type_error: 'Fundraiser ID must be of type string.',
+//     })
+//     .min(1, 'Fundraiser ID cannot be empty.'),
+//   message: z
+//     .string({
+//       invalid_type_error: 'Message must be of type string.',
+//     })
+//     .min(1, 'Message cannot be empty.'),
+// })
