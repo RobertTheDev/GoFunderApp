7a06282ae5831390187e8b90b994434c0dae5196
RobertTheDev <robertthedev@gmail.com>
2024-01-13 02:09:24 +0000

    feat: added user routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import charityRouter from '../modules/charity/charity.routes.js'
 import authRouter from '../modules/auth/auth.routes.js'
 import emailRouter from '../modules/email/email.routes.js'
 import uploadRouter from '../modules/upload/upload.routes.js'
+import userRouter from 'src/modules/user/user.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const appRouter = Router()
@@ -12,5 +13,6 @@ appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
 appRouter.use('/email', emailRouter)
 appRouter.use('/uploads', uploadRouter)
+appRouter.use('/users', userRouter)
 
 export default appRouter
