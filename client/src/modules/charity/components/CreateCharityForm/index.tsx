b9c903f3f528e3fb4360436773d5aaf56416e53a
RobertTheDev <robertthedev@gmail.com>
2024-01-13 10:03:23 +0000

    feat: added create charity schema

--- client/src/modules/charity/components/CreateCharityForm/index.tsx
+++ client/src/modules/charity/components/CreateCharityForm/index.tsx
@@ -1,38 +1,8 @@
-import axios from "axios";
 import { ReactElement } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
-
-const CreateCharitySchema = z.object({
-  category: z.string().min(1, { message: "Category is required." }),
-  name: z.string().min(1, { message: "Name is required." }),
-  logoUrl: z.string().min(1, { message: "Logo URL is required." }),
-  description: z.string().min(1, { message: "Description is required." }),
-});
-
-type CreateCharitySchemaType = z.infer<typeof CreateCharitySchema>;
+import useCreateCharityForm from "./useCreateCharityForm";
 
 export default function CreateCharityForm(): ReactElement {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<CreateCharitySchemaType>({
-    resolver: zodResolver(CreateCharitySchema),
-  });
-
-  const handleCreateCharity = handleSubmit(async (data) => {
-    try {
-      await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/charities/create`,
-        data
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
+  const { register, errors, handleCreateCharity } = useCreateCharityForm();
   return (
     <form onSubmit={handleCreateCharity}>
       <label htmlFor="name">Name</label>
@@ -41,9 +11,9 @@ export default function CreateCharityForm(): ReactElement {
       <label htmlFor="description">Description</label>
       <input {...register("description")} type="text" name="description" />
       {errors.description?.message && <p>{errors.description.message}</p>}
-      <label htmlFor="logoUrl">Logo URL</label>
-      <input {...register("logoUrl")} type="url" name="logoUrl" />
-      {errors.logoUrl?.message && <p>{errors.logoUrl.message}</p>}
+      <label htmlFor="logo">Logo URL</label>
+      <input {...register("logo")} type="url" name="logo" />
+      {errors.logo?.message && <p>{errors.logo.message}</p>}
       <label htmlFor="category">Category</label>
       <input {...register("category")} type="text" name="category" />
       {errors.category?.message && <p>{errors.category.message}</p>}
