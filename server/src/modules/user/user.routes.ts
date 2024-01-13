37b1833bcdcb1da7da02172ca2d37ebcc85c027d
RobertTheDev <robertthedev@gmail.com>
2024-01-13 02:48:03 +0000

    feat: defined user id route

--- server/src/modules/user/user.routes.ts
+++ server/src/modules/user/user.routes.ts
@@ -1,11 +1,12 @@
 import type { RequestHandler } from 'express'
 import { Router } from 'express'
-import { getAllUsers } from './user.controllers'
+import { getAllUsers, getUserById } from './user.controllers'
 
 // Sets up the user router.
 const userRouter = Router()
 
 // Defines the user routes.
 userRouter.get('/', getAllUsers as RequestHandler)
+userRouter.get('/:id', getUserById as RequestHandler)
 
 export default userRouter
