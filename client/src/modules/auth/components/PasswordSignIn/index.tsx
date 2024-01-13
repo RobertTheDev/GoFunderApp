2191f150ef1eb64fcf0c9b7558ea7cb3601f1891
RobertTheDev <robertthedev@gmail.com>
2024-01-13 05:10:28 +0000

    feat: created password sign in form

---
+++ client/src/modules/auth/components/PasswordSignIn/index.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react";
+import usePasswordSignInForm from "./usePasswordSignInForm";
+
+export default function PasswordSignInForm(): ReactElement {
+  const { register, handleSignIn, errors, errorMessage } =
+    usePasswordSignInForm();
+
+  return (
+    <form onSubmit={handleSignIn}>
+      <label htmlFor="email">Email</label>
+      <input {...register("email")} type="email" name="email" />
+      {errors.email?.message && <p>{errors.email.message}</p>}
+      <label htmlFor="password">Password</label>
+      <input {...register("password")} type="password" name="password" />
+      {errors.password?.message && <p>{errors.password.message}</p>}
+      {errorMessage && <p>{errorMessage}</p>}
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
