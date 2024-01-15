f17ffe7ca1b2c34c2a252bc480bbe1d603b71575
RobertTheDev <robertthedev@gmail.com>
2024-01-15 15:06:52 +0000

    feat: added select fields

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,31 +1,83 @@
-import type { User, Prisma } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 import { PrismaClient } from '@prisma/client'
 
 export class UserService {
   private readonly prisma = new PrismaClient()
 
-  async findUser(
-    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-  ): Promise<User | null> {
-    return this.prisma.user.findUnique({
+  // Returns a user from prisma database.
+  async findUser(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<{
+    id: string
+    annonymous: boolean
+    createdAt: Date
+    defaultCurrency: string
+    name: string
+    image: string | null
+    mfaType: string | null
+    totalCharitesOwned: number
+    totalDonationsAmount: number
+    totalDonationsMade: number
+    totalFundraisersOwned: number
+  } | null> {
+    return await this.prisma.user.findUnique({
       where: userWhereUniqueInput,
+      select: {
+        id: true,
+        annonymous: true,
+        createdAt: true,
+        defaultCurrency: true,
+        image: true,
+        mfaType: true,
+        name: true,
+        totalCharitesOwned: true,
+        totalDonationsAmount: true,
+        totalDonationsMade: true,
+        totalFundraisersOwned: true,
+      },
     })
   }
 
+  // Returns all users from prisma database.
   async findUsers(params: {
     skip?: number
     take?: number
     cursor?: Prisma.UserWhereUniqueInput
     where?: Prisma.UserWhereInput
     orderBy?: Prisma.UserOrderByWithRelationInput
-  }): Promise<User[]> {
+  }): Promise<
+    Array<{
+      id: string
+      annonymous: boolean
+      createdAt: Date
+      defaultCurrency: string
+      name: string
+      image: string | null
+      mfaType: string | null
+      totalCharitesOwned: number
+      totalDonationsAmount: number
+      totalDonationsMade: number
+      totalFundraisersOwned: number
+    }>
+  > {
     const { skip, take, cursor, where, orderBy } = params
-    return this.prisma.user.findMany({
+    return await this.prisma.user.findMany({
       skip,
       take,
       cursor,
       where,
       orderBy,
+      select: {
+        id: true,
+        annonymous: true,
+        createdAt: true,
+        defaultCurrency: true,
+        image: true,
+        mfaType: true,
+        name: true,
+        totalCharitesOwned: true,
+        totalDonationsAmount: true,
+        totalDonationsMade: true,
+        totalFundraisersOwned: true,
+      },
     })
   }
 }
