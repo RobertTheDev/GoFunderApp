f599ae1cc88e0b6bf50b37889cae99367862c44c
RobertTheDev <robertthedev@gmail.com>
2024-01-15 15:22:54 +0000

    feat: added donation routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import charityRouter from '../modules/charity/charity.routes.js'
 import authRouter from '../modules/auth/auth.routes.js'
+import charityRouter from '../modules/charity/charity.routes.js'
+import donationRouter from '../modules/donation/donation.routes.js'
 import emailRouter from '../modules/email/email.routes.js'
 import uploadRouter from '../modules/upload/upload.routes.js'
-import userRouter from 'src/modules/user/user.routes.js'
+import userRouter from '../modules/user/user.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const appRouter = Router()
@@ -11,6 +12,7 @@ const appRouter = Router()
 // Defines app routes.
 appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
+appRouter.use('/donations', donationRouter)
 appRouter.use('/email', emailRouter)
 appRouter.use('/uploads', uploadRouter)
 appRouter.use('/users', userRouter)
