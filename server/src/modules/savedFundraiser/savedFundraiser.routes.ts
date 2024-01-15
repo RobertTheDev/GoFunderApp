39eac92d549974b09b8d002ee5b7a2de8b2fcfcd
RobertTheDev <robertthedev@gmail.com>
2024-01-15 18:10:37 +0000

    feat: created saved fundraiser routes

--- server/src/modules/savedFundraiser/savedFundraiser.routes.ts
+++ server/src/modules/savedFundraiser/savedFundraiser.routes.ts
@@ -1,5 +1,13 @@
-import { Router } from 'express'
+import { type RequestHandler, Router } from 'express'
+import { getUserSavedFundraisers } from './controllers/getUserSavedFundraisers'
+import { saveFundraiser } from './controllers/saveFundraiser'
 
-const savedFundraiserRouter = Router()
+const savedFundraiserRouter: Router = Router()
+
+savedFundraiserRouter.get('/', getUserSavedFundraisers as RequestHandler)
+savedFundraiserRouter.post(
+  '/save/:fundraiserId',
+  saveFundraiser as RequestHandler,
+)
 
 export default savedFundraiserRouter
