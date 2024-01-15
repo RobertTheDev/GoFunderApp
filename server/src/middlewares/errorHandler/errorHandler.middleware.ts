29bae54673f7e5fc027afd97c7d5b7f56e336ca3
RobertTheDev <robertthedev@gmail.com>
2024-01-15 15:07:44 +0000

    feat: created error middleware

---
+++ server/src/middlewares/errorHandler/errorHandler.middleware.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from 'express'
+import { getReasonPhrase } from 'http-status-codes'
+import winstonLogger from 'src/utils/winston/winstonLogger'
+
+const errorHandler = (
+  error: any,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+): void => {
+  // Error handling middleware functionality
+  winstonLogger.error(error.message) // log the error
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const status: number = error.status || 400
+  // send back an easily understandable error message to the caller
+  response.status(status).json({
+    success: false,
+    status: getReasonPhrase(status),
+    message: error.message,
+    data: null,
+  })
+}
+
+export default errorHandler
