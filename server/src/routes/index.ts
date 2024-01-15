b47b951bc1834e197cd4d77b9b564155c8b1fdb1
RobertTheDev <robertthedev@gmail.com>
2024-01-15 15:30:25 +0000

    feat: added charity follower router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import authRouter from '../modules/auth/auth.routes.js'
 import charityRouter from '../modules/charity/charity.routes.js'
+import charityFollowerRouter from '../modules/charityFollower/charityFollower.routes.js'
+import charityOwnerRouter from '../modules/charityOwner/charityOwner.routes.js'
 import donationRouter from '../modules/donation/donation.routes.js'
 import emailRouter from '../modules/email/email.routes.js'
+import fundraiserRouter from '../modules/fundraiser/fundraiser.routes.js'
+import savedFundraiserRouter from '../modules/savedFundraiser/savedFundraiser.routes.js'
 import uploadRouter from '../modules/upload/upload.routes.js'
 import userRouter from '../modules/user/user.routes.js'
 
@@ -12,8 +16,12 @@ const appRouter = Router()
 // Defines app routes.
 appRouter.use('/auth', authRouter)
 appRouter.use('/charities', charityRouter)
+appRouter.use('/charity-followers', charityFollowerRouter)
+appRouter.use('/charity-owners', charityOwnerRouter)
 appRouter.use('/donations', donationRouter)
 appRouter.use('/email', emailRouter)
+appRouter.use('/fundraisers', fundraiserRouter)
+appRouter.use('/saved-fundraisers', savedFundraiserRouter)
 appRouter.use('/uploads', uploadRouter)
 appRouter.use('/users', userRouter)
 
