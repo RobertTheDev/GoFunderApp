8888e8068230674c758dd21a071156993a3ebc5a
RobertTheDev <robertthedev@gmail.com>
2024-01-16 09:25:26 +0000

    feat: created send email verification controller

---
+++ server/src/modules/email/controllers/sendEmailVerification.ts
@@ -0,0 +1,29 @@
+import sgMail from '@sendgrid/mail'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import winstonLogger from '../../../utils/winston/winstonLogger'
+import type { Request, Response } from 'express'
+
+sgMail.setApiKey(String(process.env.SENDGRID_API_KEY))
+
+export default async function sendEmail(
+  _req: Request,
+  res: Response,
+): Promise<void> {
+  try {
+    await sgMail.send({
+      to: 'robertthedev@gmail.com',
+      from: 'roberthawker16@gmail.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    })
+
+    res.status(StatusCodes.OK).json({ message: 'Sent email.' })
+  } catch (error) {
+    winstonLogger.error(error)
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: ReasonPhrases.INTERNAL_SERVER_ERROR,
+    })
+  }
+}
