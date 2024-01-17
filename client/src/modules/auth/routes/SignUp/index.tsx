22037eb4d114d129ade63ec477f90a33e88e84ae
RobertTheDev <robertthedev@gmail.com>
2024-01-17 17:17:02 +0000

    feat: created route

---
+++ client/src/modules/auth/routes/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import PasswordSignUpForm from "../../components/PasswordSignUp";
+
+export default function SignUpRoute(): ReactElement {
+  return (
+    <div>
+      <PasswordSignUpForm />
+    </div>
+  );
+}
---
+++ client/src/modules/auth/routes/Totp/index.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+// import TotpSetupForm from "../../components/TotpSetupForm";
+import OtpEmailForm from "../../components/OtpEmailForm";
+
+export default function TotpRoute(): ReactElement {
+  return <OtpEmailForm />;
+}
