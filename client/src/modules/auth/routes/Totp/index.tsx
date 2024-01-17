57689da5e098be954d57ddbe18d15b000e41b8fc
RobertTheDev <robertthedev@gmail.com>
2024-01-17 18:15:47 +0000

    ci: deleted files

--- client/src/modules/auth/routes/Totp/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-// import TotpSetupForm from "../../components/TotpSetupForm";
-import OtpEmailForm from "../../components/OtpEmailForm";
-
-export default function TotpRoute(): ReactElement {
-  return <OtpEmailForm />;
-}
