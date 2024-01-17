0bf46e615c6b45aea29856ab8672797a7483aebd
RobertTheDev <robertthedev@gmail.com>
2024-01-17 17:17:46 +0000

    feat: added new auth routes

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -2,9 +2,7 @@ import type { RequestHandler } from 'express'
 import { Router } from 'express'
 import { getAuthenticatedUser, signOut } from './auth.controllers.js'
 import emailPasswordRouter from './emailPassword/emailPassword.routes.js'
-import magicLinkRouter from './magicLink/magicLink.routes.js'
 import totpRouter from './totp/totp.routes.js'
-import otpRouter from './otp/otp.routes.js'
 import oauthRouter from './oauth/oauth.routes.js'
 
 // Sets up the charity router.
@@ -15,9 +13,7 @@ authRouter.get('/user', getAuthenticatedUser as RequestHandler)
 authRouter.post('/sign-out', signOut as RequestHandler)
 // Configures auth module routers.
 authRouter.use('/', emailPasswordRouter)
-authRouter.use('/magic-link', magicLinkRouter)
 authRouter.use('/oauth', oauthRouter)
-authRouter.use('/otp', otpRouter)
 authRouter.use('/totp', totpRouter)
 
 export default authRouter
