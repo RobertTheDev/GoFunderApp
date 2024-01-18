afc9ba061e73849ea94b4829d93ee1cc520b0338
RobertTheDev <robertthedev@gmail.com>
2024-01-18 17:57:52 +0000

    refactor: moved to validators directory

--- client/src/modules/charity/validators/createCharity.schema.ts
+++ client/src/modules/charity/validators/createCharity.schema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-export type CreateCharitySchemaType = z.infer<typeof CreateCharitySchema>;
+export type CreateCharitySchemaType = z.infer<typeof createCharitySchema>;
 
-const CreateCharitySchema = z.object({
+const createCharitySchema = z.object({
   category: z.string().min(1, { message: "Category is required." }),
   name: z.string().min(1, { message: "Name is required." }),
   logo: z.string().min(1, { message: "Logo URL is required." }),
   description: z.string().min(1, { message: "Description is required." }),
 });
 
-export default CreateCharitySchema;
+export default createCharitySchema;
