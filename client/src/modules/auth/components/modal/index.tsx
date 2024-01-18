3b4248d8ef5dd9dec9fb576238eb7663a6fff247
RobertTheDev <robertthedev@gmail.com>
2024-01-18 06:20:07 +0000

    feat: set up outside click

--- client/src/modules/auth/components/modal/index.tsx
+++ client/src/modules/auth/components/modal/index.tsx
@@ -1,15 +1,24 @@
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useRef } from "react";
 import styles from "./styles.module.scss";
 import { AuthContext } from "../../contexts/AuthContext/context";
+import { useOnClickOutside } from "usehooks-ts";
 
 export default function AuthModal(): ReactElement {
   const { toggleAuthModal } = useContext(AuthContext);
 
+  const authModalRef = useRef<HTMLDivElement>(null);
+
+  const handleToggleAuthModal: () => void = () => {
+    toggleAuthModal(null);
+  };
+
+  useOnClickOutside(authModalRef, handleToggleAuthModal);
+
   return (
     <div className={styles.modalWrapper}>
-      <div className={styles.modalContainer}>
+      <div ref={authModalRef} className={styles.modalContainer}>
         <p>Auth</p>
-        <button type="button" onClick={() => toggleAuthModal(null)}>
+        <button type="button" onClick={handleToggleAuthModal}>
           Close
         </button>
       </div>
