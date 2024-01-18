c4e941cd63b445dbc4226bd9d45b19cb083ae5bd
RobertTheDev <robertthedev@gmail.com>
2024-01-18 04:37:48 +0000

    feat: created auth context

---
+++ client/src/modules/auth/contexts/AuthContext/context.tsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+import { AuthContextType } from "./AuthContextType";
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  isPending: false,
+  error: null,
+  authModal: { active: false, formType: null },
+  toggleAuthModal: () => {},
+});
