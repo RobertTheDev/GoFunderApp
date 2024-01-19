3dc43f38a75d70fac826ef07b5f00e9316b61349
RobertTheDev <robertthedev@gmail.com>
2024-01-19 15:59:45 +0000

    feat: added footer links

--- client/src/modules/layout/components/Footer/index.tsx
+++ client/src/modules/layout/components/Footer/index.tsx
@@ -1,10 +1,26 @@
 import { ReactElement } from "react";
 import styles from "./styles.module.scss";
 
+const footerLinks = [
+  { url: "/privacy-policy", name: "Privacy Policy" },
+  { url: "/terms-and-conditions", name: "Terms and Conditions" },
+  { url: "/about", name: "About" },
+];
+
 export default function Footer(): ReactElement {
   return (
     <footer className={styles.footerContainer}>
-      <p>Footer</p>
+      <div>©{new Date().getFullYear()} GoFunder</div>
+      <div>
+        {footerLinks.map((footerLink) => {
+          return (
+            <a key={footerLink.url} href={footerLink.url}>
+              {footerLink.name}
+            </a>
+          );
+        })}
+      </div>
+      <div></div>
     </footer>
   );
 }
