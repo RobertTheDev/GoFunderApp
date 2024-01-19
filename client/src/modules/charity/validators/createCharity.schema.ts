e37edd9694e6ea4cfb7df9329f6a44350a5cb840
RobertTheDev <robertthedev@gmail.com>
2024-01-19 07:17:50 +0000

    feat: added create charity fields

--- client/src/modules/charity/validators/createCharity.schema.ts
+++ client/src/modules/charity/validators/createCharity.schema.ts
@@ -3,10 +3,32 @@ import { z } from "zod";
 export type CreateCharitySchemaType = z.infer<typeof createCharitySchema>;
 
 const createCharitySchema = z.object({
-  category: z.string().min(1, { message: "Category is required." }),
-  name: z.string().min(1, { message: "Name is required." }),
-  logo: z.string().min(1, { message: "Logo URL is required." }),
-  description: z.string().min(1, { message: "Description is required." }),
+  category: z
+    .string({
+      invalid_type_error: "Category must be of type string.",
+      required_error: "Category is required.",
+    })
+
+    .min(1, { message: "Category is required." }),
+  name: z
+    .string({
+      invalid_type_error: "Name must be of type string.",
+      required_error: "Name is required.",
+    })
+    .min(1, { message: "Name is required." }),
+  logo: z
+    .string({
+      invalid_type_error: "Logo URL must be of type string.",
+      required_error: "Logo is required.",
+    })
+    .url("Logo must a valid url.")
+    .min(1, { message: "Logo URL is required." }),
+  description: z
+    .string({
+      invalid_type_error: "Description must be of type string.",
+      required_error: "Description is required.",
+    })
+    .min(1, { message: "Description is required." }),
 });
 
 export default createCharitySchema;
