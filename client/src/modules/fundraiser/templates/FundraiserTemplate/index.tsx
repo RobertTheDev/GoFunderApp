ff78186e9f4a7026ad305fee4a1db309ad7773c2
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:59:30 +0000

    feat: created fundraiser template

---
+++ client/src/modules/fundraiser/templates/FundraiserTemplate/index.tsx
@@ -0,0 +1,43 @@
+import { ReactElement } from "react";
+import IFundraiser from "../../../../interfaces/Fundraiser";
+
+export default function FundraiserTemplate({
+  fundraiser,
+}: {
+  fundraiser: IFundraiser | null;
+}): ReactElement {
+  if (fundraiser == null) {
+    return <p>noo</p>;
+  }
+
+  return (
+    <div>
+      <p>{fundraiser.name}</p>
+      <p>{fundraiser.category}</p>
+      <img src={fundraiser.image} alt={fundraiser.name} />
+      <p>{new Date(fundraiser.createdAt).getDate()}</p>
+      <p>{new Date(fundraiser.deadlineDate).getDate()}</p>
+      <p>{fundraiser.description}</p>
+      <p>{fundraiser.headline}</p>
+      <p>{fundraiser.target}</p>
+      <p>{fundraiser.totalDonations}</p>
+      <p>{fundraiser.totalRaised}</p>
+      <h3>Donations</h3>
+      {fundraiser.donations.map((donation) => {
+        return (
+          <div key={donation.amount}>
+            <p>{donation.amount}</p>
+            <div>
+              {donation.user.image ? (
+                <img src={donation.user.image} alt={donation.user.name} />
+              ) : (
+                <p>No image</p>
+              )}
+              <p>{donation.user.name}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
