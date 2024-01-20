2d2ad109a0f3f6865c27ba0d0ee5a1366fee7b68
RobertTheDev <robertthedev@gmail.com>
2024-01-20 21:41:09 +0000

    refactor: renamed directory to features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactElement } from "react";
-import AuthProvider from "./modules/auth/contexts/AuthContext/provider";
+import AuthProvider from "./features/auth/contexts/AuthContext/provider";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import PageLayout from "../modules/layout/components/PageLayout";
-import authRouter from "../modules/auth/routes/auth.routes";
-import charityRouter from "../modules/charity/routes/charity.routes";
-import HomePage from "../modules/home/pages/HomePage";
-import infoRouter from "../modules/info/routes/info.routes";
-import fundraiserRouter from "../modules/fundraiser/routes/fundraiser.routes";
-import userRouter from "../modules/user/routes/user.routes";
-import savedFundraiserRouter from "../modules/savedFundraiser/routes/savedFundraiser.routes";
+import PageLayout from "../features/layout/components/PageLayout";
+import authRouter from "../features/auth/routes/auth.routes";
+import charityRouter from "../features/charity/routes/charity.routes";
+import HomePage from "../features/home/pages/HomePage";
+import infoRouter from "../features/info/routes/info.routes";
+import fundraiserRouter from "../features/fundraiser/routes/fundraiser.routes";
+import userRouter from "../features/user/routes/user.routes";
+import savedFundraiserRouter from "../features/savedFundraiser/routes/savedFundraiser.routes";
 
 const router = createBrowserRouter([
   {
