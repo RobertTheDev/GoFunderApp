d0daa124751c8a87ddfce38ce1a98500dd87cf97
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:51:53 +0000

    feat: added validation schema type

--- client/src/modules/auth/service/auth.service.ts
+++ client/src/modules/auth/service/auth.service.ts
@@ -1,16 +1,30 @@
 import axios, { AxiosResponse } from "axios";
 import ApiResponse from "../../../interfaces/ApiResponse";
 import IUser from "../../../interfaces/User";
+import { PasswordSignUpSchemaType } from "../validators/passwordSignUp.schema";
+import { PasswordSignInSchemaType } from "../validators/passwordSignIn.schema";
 
 interface UserApiResponse extends ApiResponse {
   data: IUser;
 }
 
-export async function passwordSignUpUser(
-  data: any
+export async function signUpWithPassword(
+  data: PasswordSignUpSchemaType
 ): Promise<AxiosResponse<UserApiResponse>> {
   return await axios.post(
-    `${process.env.REACT_APP_API_URL}/api/auth/sign-up`,
+    `${process.env.REACT_APP_API_URL}/auth/sign-up`,
+    data,
+    {
+      withCredentials: true,
+    }
+  );
+}
+
+export async function signInWithPassword(
+  data: PasswordSignInSchemaType
+): Promise<AxiosResponse<UserApiResponse>> {
+  return await axios.post(
+    `${process.env.REACT_APP_API_URL}/auth/sign-in`,
     data,
     {
       withCredentials: true,
@@ -19,11 +33,8 @@ export async function passwordSignUpUser(
 }
 
 export async function getAuthenticatedUser() {
-  const user = await axios.get(
-    `${process.env.REACT_APP_API_URL}/api/auth/user`,
-    {
-      withCredentials: true,
-    }
-  );
+  const user = await axios.get(`${process.env.REACT_APP_API_URL}/auth/user`, {
+    withCredentials: true,
+  });
   return user.data.data;
 }
