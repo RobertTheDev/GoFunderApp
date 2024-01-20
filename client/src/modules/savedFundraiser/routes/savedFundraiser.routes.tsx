ee366ec1b4f8be62f8732dec3648f4051e3dd852
RobertTheDev <robertthedev@gmail.com>
2024-01-20 20:42:27 +0000

    feat: defined fundraiser routes

---
+++ client/src/modules/savedFundraiser/routes/savedFundraiser.routes.tsx
@@ -0,0 +1,10 @@
+import SavedFundraisersPage from "../pages/SavedFundraisers";
+
+const savedFundraiserRouter = [
+  {
+    path: "/saved-fundraisers",
+    element: <SavedFundraisersPage />,
+  },
+];
+
+export default savedFundraiserRouter;
