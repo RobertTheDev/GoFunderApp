68ecd0e95bef841e1d700ac5e89ff664ed008392
RobertTheDev <robertthedev@gmail.com>
2024-01-20 18:38:11 +0000

    ci: removed react confetti

--- client/src/modules/fundraiser/components/CreateFundraiserForm/index.tsx
+++ client/src/modules/fundraiser/components/CreateFundraiserForm/index.tsx
@@ -1,19 +1,9 @@
 import { ReactElement } from "react";
-import useWindowSize from "react-use/lib/useWindowSize";
-import Confetti from "react-confetti";
 
 export default function CreateFundraiserForm(): ReactElement {
-  const { width, height } = useWindowSize();
-
   return (
     <div>
       <p>Create Fundraiser Fom</p>
-      <Confetti
-        style={{ zIndex: 44444 }}
-        width={width}
-        height={height}
-        numberOfPieces={400}
-      />
     </div>
   );
 }
