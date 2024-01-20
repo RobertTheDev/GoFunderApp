92ae29e1639a3ca984f830518f10880ff0fb8ed0
RobertTheDev <robertthedev@gmail.com>
2024-01-20 18:03:10 +0000

    feat: added charity template

--- client/src/modules/charity/views/Charity/index.tsx
+++ client/src/modules/charity/views/Charity/index.tsx
@@ -1,8 +1,9 @@
-import { Link, useParams } from "react-router-dom";
-import Seo from "../../../seo/components/Seo";
+import { useParams } from "react-router-dom";
+
 import { useQuery } from "@tanstack/react-query";
 import { ReactElement } from "react";
 import { getCharityById } from "../../service/charity.service";
+import CharityTemplate from "../../templates/CharityTemplate";
 
 export default function CharityRoute(): ReactElement {
   const { id } = useParams();
@@ -16,16 +17,5 @@ export default function CharityRoute(): ReactElement {
 
   if (error) return <p>An error has occurred: + {error.message}</p>;
 
-  if (!data.data.data) return <p>No charity found.</p>;
-
-  return (
-    <div>
-      <Seo title={data.data.data.name} />
-      <Link to={"update"}>Update Charity</Link>
-      <Link to={"delete"}>Delete Charity</Link>
-      <p>{data.data.data.name}</p>
-      <p>{data.data.data.description}</p>
-      <img src={data.data.data.logo} alt={data.data.data.name} />
-    </div>
-  );
+  return <CharityTemplate charity={data.data.data} />;
 }
