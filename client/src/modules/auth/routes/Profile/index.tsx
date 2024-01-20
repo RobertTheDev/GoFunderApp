e5a08707b93360b075d553a9751b4e2224ad5c1c
RobertTheDev <robertthedev@gmail.com>
2024-01-20 18:06:09 +0000

    feat: added use query

--- client/src/modules/auth/routes/Profile/index.tsx
+++ client/src/modules/auth/routes/Profile/index.tsx
@@ -1,43 +1,12 @@
 import axios from "axios";
 import Seo from "../../../seo/components/Seo";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { ReactElement } from "react";
 import IUser from "../../../../interfaces/User";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 
 export default function ProfileRoute(): ReactElement {
   const navigate: NavigateFunction = useNavigate();
-
-  async function signOut() {
-    try {
-      await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/auth/sign-out`,
-        {},
-        {
-          withCredentials: true,
-        }
-      );
-
-      navigate("/");
-
-      window.location.reload();
-    } catch (error: any) {
-      console.error("Sign-out error:", error);
-    }
-  }
-
-  const queryClient = useQueryClient();
-
-  const mutation = useMutation({
-    mutationFn: signOut,
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["getProfileData"] });
-    },
-    onError: () => {
-      console.log("error");
-    },
-  });
 
   const { isPending, error, data } = useQuery<IUser | null>({
     queryKey: ["getProfileData"],
@@ -66,8 +35,6 @@ export default function ProfileRoute(): ReactElement {
       <p>{data?.email}</p>
 
       <img src={data?.image ? data.image : ""} alt="Avatart" />
-
-      <button onClick={() => mutation.mutate()}>Sign Out</button>
     </div>
   );
 }
