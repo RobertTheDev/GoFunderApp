c6bbbe4f9a77925ba2613c6609db0345d82a395d
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:54:56 +0000

    feat: added date fns formatter

--- client/src/modules/charity/components/CharityCard/index.tsx
+++ client/src/modules/charity/components/CharityCard/index.tsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
 import ICharity from "../../../../interfaces/Charity";
 import { ReactElement } from "react";
+import { format } from "date-fns";
+import styles from "./styles.module.scss";
 
 export default function CharityCard(charity: ICharity): ReactElement {
   return (
-    <Link to={`/charities/${charity.id}`}>
+    <Link to={`/charities/${charity.id}`} className={styles.cardContainer}>
+      <img
+        className={styles.cardImageContainer}
+        src={charity.logo}
+        alt={charity.name}
+      />
+      <p>{charity.name}</p>
+      <p>{charity.category}</p>
+      <p>
+        GoFunder charity since{" "}
+        {format(new Date(charity.createdAt), "dd MMMM yyyy")}
+      </p>
       <div>
-        <img
-          style={{ height: 400, width: 400 }}
-          src={charity.logo}
-          alt={charity.name}
-        />
-        <p>{charity.name}</p>
+        <button type="button">Follow</button>
+        <button type="button">View</button>
       </div>
     </Link>
   );
--- client/src/modules/fundraiser/components/FundraiserCard/index.tsx
+++ client/src/modules/fundraiser/components/FundraiserCard/index.tsx
@@ -1,9 +1,35 @@
 import { ReactElement } from "react";
+import IFundraiser from "../../../../interfaces/Fundraiser";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
+import { format } from "date-fns";
 
-export default function FundraiserCard(): ReactElement {
+export default function FundraiserCard(fundraiser: IFundraiser): ReactElement {
   return (
-    <div>
-      <p>Fundraiser Card</p>
-    </div>
+    <Link className={styles.cardContainer} to={`/fundraisers/${fundraiser.id}`}>
+      <img
+        className={styles.cardImageContainer}
+        src={fundraiser.image}
+        alt="ddd"
+      />
+      <p>{fundraiser.name}</p>
+      <p>
+        Closes on{" "}
+        {format(new Date(fundraiser.deadlineDate).getDate(), "d MMMM yyyy")}
+      </p>
+      <p>
+        Created on{" "}
+        {format(new Date(fundraiser.createdAt).getDate(), "d MMMM yyyy")}
+      </p>
+      <p>£{fundraiser.target.toLocaleString()}</p>
+      <p>{fundraiser.category}</p>
+
+      <p>{fundraiser.headline}</p>
+
+      <p>
+        £{fundraiser.totalRaised.toLocaleString()} raised from{" "}
+        {fundraiser.totalDonations} donations.
+      </p>
+    </Link>
   );
 }
