16bc5bc8335db561b53d3b85ea20daa72209e43a
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:50:03 +0000

    feat: defined social links

---
+++ client/src/modules/layout/components/Footer/socialLinks.tsx
@@ -0,0 +1,32 @@
+import { FaFacebook, FaInstagram, FaTiktok, FaXTwitter } from "react-icons/fa6";
+
+const socialLinks = [
+  {
+    id: "1",
+    url: "https://www.facebook.com",
+    icon: <FaFacebook />,
+    name: "Facebook",
+  },
+
+  {
+    id: "2",
+    url: "https://www.instagram.com",
+    icon: <FaInstagram />,
+    name: "Instagram",
+  },
+  {
+    id: "3",
+    url: "https://www.tiktok.com",
+    icon: <FaTiktok />,
+    name: "TikTok",
+  },
+
+  {
+    id: "4",
+    url: "https://www.x.com",
+    icon: <FaXTwitter />,
+    name: "X",
+  },
+];
+
+export default socialLinks;
