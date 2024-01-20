0e7bca20cb311660d819426e46394c3ba9034fdd
RobertTheDev <robertthedev@gmail.com>
2024-01-20 18:02:39 +0000

    feat: added fundraiser template

--- client/src/modules/fundraiser/routes/Fundraiser/index.tsx
+++ client/src/modules/fundraiser/routes/Fundraiser/index.tsx
@@ -1,9 +1,26 @@
 import { ReactElement } from "react";
+import Seo from "../../../seo/components/Seo";
+import { useQuery } from "@tanstack/react-query";
+import { getFundraiserById } from "../../service/fundraiser.service";
+import { useParams } from "react-router-dom";
+import FundraiserTemplate from "../../templates/FundraiserTemplate";
 
 export default function Fundraiser(): ReactElement {
+  const { id } = useParams();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getFundraiserByIdData"],
+    queryFn: () => getFundraiserById(String(id)),
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: + {error.message}</p>;
+
   return (
-    <div>
-      <p>Fundraiser</p>
-    </div>
+    <>
+      <Seo title="Fundraiser" />
+      <FundraiserTemplate fundraiser={data.data.data} />
+    </>
   );
 }
