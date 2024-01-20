d79fca5da1818079e603829db7c360ec67b78a2d
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:49:26 +0000

    feat: defined get fundraisers by charity route

--- server/src/modules/fundraiser/fundraiser.routes.ts
+++ server/src/modules/fundraiser/fundraiser.routes.ts
@@ -4,12 +4,18 @@ import {
   getAllFundraisers,
   getFundraiserById,
 } from './fundraiser.controllers.js'
+import { getFundraisersByCategory } from './controllers/getFundraisersByCategory.js'
 
 // Sets up the fundraiser router.
 const fundraiserRouter = Router()
 
 // Defines the fundraiser routes.
 fundraiserRouter.get('/', getAllFundraisers as RequestHandler)
+fundraiserRouter.get(
+  '/category/:category',
+  getFundraisersByCategory as RequestHandler,
+)
+
 fundraiserRouter.get('/:id', getFundraiserById as RequestHandler)
 
 export default fundraiserRouter
