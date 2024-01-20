990e6ce0391e2aa6a4d9ef84fafad96112e967ee
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:58:09 +0000

    feat: added react confetti

--- client/src/modules/fundraiser/components/CreateFundraiserForm/index.tsx
+++ client/src/modules/fundraiser/components/CreateFundraiserForm/index.tsx
@@ -1,9 +1,19 @@
 import { ReactElement } from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 export default function CreateFundraiserForm(): ReactElement {
+  const { width, height } = useWindowSize();
+
   return (
     <div>
-      <p>Create Fundraiser Form</p>
+      <p>Create Fundraiser Fom</p>
+      <Confetti
+        style={{ zIndex: 44444 }}
+        width={width}
+        height={height}
+        numberOfPieces={400}
+      />
     </div>
   );
 }
