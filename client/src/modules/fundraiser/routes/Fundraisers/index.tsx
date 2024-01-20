9b6e0f13574bf5134cf33b031eb92bfb48dfbd2f
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:58:39 +0000

    feat: fetched and mapped fundraisers data

--- client/src/modules/fundraiser/routes/Fundraisers/index.tsx
+++ client/src/modules/fundraiser/routes/Fundraisers/index.tsx
@@ -1,9 +1,29 @@
 import { ReactElement } from "react";
+import Seo from "../../../seo/components/Seo";
+import { useQuery } from "@tanstack/react-query";
+import { getFundraisers } from "../../service/fundraiser.service";
+import FundraiserCard from "../../components/FundraiserCard";
+import styles from "./styles.module.scss";
 
-export default function Fundraisers(): ReactElement {
+export default function FundraisersRoute(): ReactElement {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getFundraisersData"],
+    queryFn: getFundraisers,
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: + {error.message}</p>;
+
   return (
-    <div>
-      <p>Fundraisers</p>
-    </div>
+    <>
+      <Seo title="Fundraisers" />
+
+      <div className={styles.fundraiserCardsContainer}>
+        {data.data.data.map((fundraiser) => {
+          return <FundraiserCard {...fundraiser} key={fundraiser.id} />;
+        })}
+      </div>
+    </>
   );
 }
