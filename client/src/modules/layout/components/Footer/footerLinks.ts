f1c87862ee4b2f3825cfac564e635eb3ae8754e9
RobertTheDev <robertthedev@gmail.com>
2024-01-20 17:50:17 +0000

    feat: defined footer links

---
+++ client/src/modules/layout/components/Footer/footerLinks.ts
@@ -0,0 +1,11 @@
+const footerLinks: {
+  id: string;
+  url: string;
+  name: string;
+}[] = [
+  { id: "1", url: "/privacy-policy", name: "Privacy Policy" },
+  { id: "2", url: "/terms-and-conditions", name: "Terms and Conditions" },
+  { id: "3", url: "/about", name: "About" },
+];
+
+export default footerLinks;
