1c4cc95128be8e3ed840331464849f53a3cf92b4
RobertTheDev <robertthedev@gmail.com>
2024-01-20 21:17:09 +0000

    ci: removed profile route

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import PageLayout from "../modules/layout/components/PageLayout";
-import ProfileRoute from "../modules/auth/routes/Profile";
-import authRouter from "../modules/auth/router";
+import authRouter from "../modules/auth/routes/auth.routes";
 import charityRouter from "../modules/charity/routes/charity.routes";
 import HomePage from "../modules/home/pages/HomePage";
 import infoRouter from "../modules/info/routes/info.routes";
@@ -25,11 +24,6 @@ const router = createBrowserRouter([
         element: <p>Not found</p>,
       },
       { path: "/", element: <HomePage /> },
-
-      {
-        path: "/profile",
-        element: <ProfileRoute />,
-      },
     ],
   },
 ]);
