76aee366016cbe58f0499efba6b1a9721ce77fcc
RobertTheDev <robertthedev@gmail.com>
2024-01-20 18:01:24 +0000

    feat: added profile menu component

--- client/src/modules/layout/components/Header/index.tsx
+++ client/src/modules/layout/components/Header/index.tsx
@@ -2,9 +2,11 @@ import { ReactElement, useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
 import { AuthContext } from "../../../auth/contexts/AuthContext/context";
+import { FaRegBell, FaRegHeart, FaRegMoon, FaRegSun } from "react-icons/fa6";
+import ProfileMenu from "../ProfileMenu";
 
 export default function Header(): ReactElement {
-  const { authModal, toggleAuthModal } = useContext(AuthContext);
+  const { authModal, toggleAuthModal, user } = useContext(AuthContext);
 
   return (
     <div className={styles.headerContainer}>
@@ -16,17 +18,44 @@ export default function Header(): ReactElement {
         <Link to={"/"}>Home</Link>
         <Link to={"/charities"}>Find Charities</Link>
         <Link to={"/fundraisers"}>Find Fundraisers</Link>
-        {/* <Link to={"/profile"}>Profile</Link> */}
+        <Link to={"/start-fundraising"}>Start Fundraising</Link>
       </div>
       <div className={styles.headerRight}>
-        <button type="button">Start Fundraising</button>
-
-        <button
-          type="button"
-          onClick={() => toggleAuthModal(!authModal.active, "signIn")}
-        >
-          Sign In
-        </button>
+        {user ? (
+          <div>
+            <FaRegBell />
+            <FaRegHeart />
+            <FaRegSun />
+            <FaRegMoon />
+            <Link to={"/profile"}>
+              <div className={styles.headerAvatarContainer}>
+                {user.image ? (
+                  <img
+                    className={styles.headerAvatarImage}
+                    src={user.image}
+                    alt="Avatar"
+                  />
+                ) : (
+                  <img
+                    className={styles.headerAvatarImage}
+                    src="https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
+                    alt="Avatar"
+                  />
+                )}
+              </div>
+            </Link>
+            <ProfileMenu />
+          </div>
+        ) : (
+          <div>
+            <button
+              type="button"
+              onClick={() => toggleAuthModal(!authModal.active, "signIn")}
+            >
+              Sign In
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
