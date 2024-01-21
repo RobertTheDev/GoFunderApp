9659d2632e49bd2655029448d1712b3d6f3ab86e
RobertTheDev <robertthedev@gmail.com>
2024-01-21 22:17:23 +0000

    feat: added description field

--- client/src/features/common/Seo/index.tsx
+++ client/src/features/common/Seo/index.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from "react";
 import { Helmet } from "react-helmet-async";
+import ISeo from "../../../interfaces/Seo";
 
-export default function Seo({ title }: { title: string }): ReactElement {
+export default function Seo({ description, title }: ISeo): ReactElement {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="description" content={description}></meta>
       <link rel="canonical" href="https://www.tacobell.com/" />
     </Helmet>
   );
--- client/src/interfaces/Seo.ts
+++ client/src/interfaces/Seo.ts
@@ -1,3 +1,4 @@
 export default interface ISeo {
+  description: string;
   title: string;
 }
