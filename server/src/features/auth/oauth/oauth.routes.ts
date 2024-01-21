05f1846502e8514e5b05ba6fdd1f64a70097e043
RobertTheDev <robertthedev@gmail.com>
2024-01-21 14:27:45 +0000

    feat: added login with amazon handler

--- server/src/features/auth/oauth/oauth.routes.ts
+++ server/src/features/auth/oauth/oauth.routes.ts
@@ -1,12 +1,12 @@
 import { type RequestHandler, Router } from 'express'
-import { signInWithAmazon } from './controllers/amazonOauth'
+import { signInWithAmazonHandler } from './controllers/amazonOauth'
 import { signInWithFacebook } from './controllers/facebookOauth'
 import { signInWithGithub } from './controllers/githubOauth'
 import { signInWithGoogle } from './controllers/googleOauth'
 
 const oauthRouter = Router()
 
-oauthRouter.get('/amazon/:code', signInWithAmazon as RequestHandler)
+oauthRouter.get('/amazon/:code', signInWithAmazonHandler as RequestHandler)
 oauthRouter.get('/facebook/:code', signInWithFacebook as RequestHandler)
 oauthRouter.get('/github/:code', signInWithGithub as RequestHandler)
 oauthRouter.get('/google/:code', signInWithGoogle as RequestHandler)
