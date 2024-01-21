7c8c50f6e369f3827372084fa29d01eceab1a8af
RobertTheDev <robertthedev@gmail.com>
2024-01-21 16:21:40 +0000

    feat: defined oauth routes

--- server/src/features/auth/oauth/oauth.routes.ts
+++ server/src/features/auth/oauth/oauth.routes.ts
@@ -1,14 +1,14 @@
 import { type RequestHandler, Router } from 'express'
-import { signInWithAmazonHandler } from './controllers/amazonOauth'
-import { signInWithFacebook } from './controllers/facebookOauth'
-import { signInWithGithub } from './controllers/githubOauth'
-import { signInWithGoogle } from './controllers/googleOauth'
+import signInWithAmazonHandler from './controllers/amazonOauth'
+import signInWithFacebookHandler from './controllers/facebookOauth'
+import signInWithGithubHandler from './controllers/githubOauth'
+import signInWithGoogleHandler from './controllers/googleOauth'
 
 const oauthRouter = Router()
 
 oauthRouter.get('/amazon/:code', signInWithAmazonHandler as RequestHandler)
-oauthRouter.get('/facebook/:code', signInWithFacebook as RequestHandler)
-oauthRouter.get('/github/:code', signInWithGithub as RequestHandler)
-oauthRouter.get('/google/:code', signInWithGoogle as RequestHandler)
+oauthRouter.get('/facebook/:code', signInWithFacebookHandler as RequestHandler)
+oauthRouter.get('/github/:code', signInWithGithubHandler as RequestHandler)
+oauthRouter.get('/google/:code', signInWithGoogleHandler as RequestHandler)
 
 export default oauthRouter
