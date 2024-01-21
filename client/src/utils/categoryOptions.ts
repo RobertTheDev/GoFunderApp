4c1df318ce1b99ce2484f8986e19d39a663bf703
RobertTheDev <robertthedev@gmail.com>
2024-01-21 11:57:44 +0000

    feat: defined category options

---
+++ client/src/utils/categoryOptions.ts
@@ -0,0 +1,13 @@
+// These are the available allowed category options for a charity.
+const categoryOptions = [
+  "Animals and pets",
+  "Art and culture",
+  "Disability",
+  "Education",
+  "Health and medical",
+  "International aid",
+  "Local community",
+  "Sports",
+];
+
+export default categoryOptions;
