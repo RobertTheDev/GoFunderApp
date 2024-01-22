bdf564a1da1154f7039bc83a33da8e076e9ea589
RobertTheDev <robertthedev@gmail.com>
2024-01-22 13:19:07 +0000

    feat: created template

---
+++ client/src/features/donation/templates/CharityDonationsPage/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function CharityDonationsPageTemplate(): ReactElement {
+  return (
+    <div>
+      <h1>Charity Donations</h1>
+    </div>
+  );
+}
