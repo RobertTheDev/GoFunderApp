93bb70569c663131df09a82e1d629ed1dfa45495
RobertTheDev <robertthedev@gmail.com>
2024-01-22 13:20:13 +0000

    feat: created component

---
+++ client/src/features/notification/components/NotificationCard/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function NotificationCard(): ReactElement {
+  return (
+    <div>
+      <p>Notification Card</p>
+    </div>
+  );
+}
---
+++ client/src/features/notification/components/NotificationsMenu/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function NotificationsMenu(): ReactElement {
+  return (
+    <div>
+      <p>Notifications Menu</p>
+    </div>
+  );
+}
