aa5f6f4c9956202f1017d604a3d066c3eb468187
RobertTheDev <robertthedev@gmail.com>
2024-01-22 10:11:58 +0000

    feat: created and imported home page template

--- client/src/features/home/pages/HomePage/index.tsx
+++ client/src/features/home/pages/HomePage/index.tsx
@@ -1,21 +1,12 @@
 import { ReactElement } from "react";
-import Banner from "../../components/Banner";
-import CategoryFundraiserSection from "../../components/CategoryFundraiserSection";
-import CategoryMenu from "../../components/CategoryMenu";
 import Seo from "../../../common/Seo";
+import HomePageTemplate from "../../templates/HomePageTemplate";
 
 export default function HomePage(): ReactElement {
   return (
-    <div>
+    <>
       <Seo title="Home" description="" />
-      <Banner />
-      <CategoryFundraiserSection category="Disability" />
-      <CategoryFundraiserSection category="Disability" />
-      <CategoryFundraiserSection category="Disability" />
-      <CategoryMenu />
-      <CategoryFundraiserSection category="Disability" />
-      <CategoryFundraiserSection category={"Disability"} />
-      <CategoryFundraiserSection category="Disability" />
-    </div>
+      <HomePageTemplate />
+    </>
   );
 }
---
+++ client/src/features/home/templates/HomePageTemplate/index.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react";
+import Banner from "../../components/Banner";
+import CategoryFundraiserSection from "../../components/CategoryFundraiserSection";
+import CategoryMenu from "../../components/CategoryMenu";
+
+export default function HomePageTemplate(): ReactElement {
+  return (
+    <div>
+      <Banner />
+      <CategoryFundraiserSection category="Disability" />
+      <CategoryFundraiserSection category="Disability" />
+      <CategoryFundraiserSection category="Disability" />
+      <CategoryMenu />
+      <CategoryFundraiserSection category="Disability" />
+      <CategoryFundraiserSection category={"Disability"} />
+      <CategoryFundraiserSection category="Disability" />
+    </div>
+  );
+}
