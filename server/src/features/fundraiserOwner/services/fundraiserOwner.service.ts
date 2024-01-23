f96d7628f0cf5a3c9555d259dce3f4b8de3d78df
RobertTheDev <robertthedev@gmail.com>
2024-01-23 22:35:40 +0000

    feat: defined service handlers

---
+++ server/src/features/fundraiserOwner/services/fundraiserOwner.service.ts
@@ -0,0 +1,66 @@
+import type { FundraiserOwner, Prisma } from '@prisma/client'
+import prismaClient from '../../../utils/prisma/prismaClient'
+
+// Returns a fundraiser from prisma database.
+export async function findFundraiserOwner(
+  fundraiserOwnerWhereUniqueInput: Prisma.FundraiserOwnerWhereUniqueInput,
+): Promise<FundraiserOwner | null> {
+  return await prismaClient.fundraiserOwner.findUnique({
+    where: fundraiserOwnerWhereUniqueInput,
+  })
+}
+
+// Returns all fundraisers from prisma database.
+export async function findFundraiserOwners(params: {
+  skip?: number
+  take?: number
+  cursor?: Prisma.FundraiserOwnerWhereUniqueInput
+  where?: Prisma.FundraiserOwnerWhereInput
+  orderBy?: Prisma.FundraiserOwnerOrderByWithRelationInput
+}): Promise<FundraiserOwner[]> {
+  const { skip, take, cursor, where, orderBy } = params
+  return await prismaClient.fundraiserOwner.findMany({
+    skip,
+    take,
+    cursor,
+    where,
+    orderBy,
+  })
+}
+
+export async function createFundraiserOwner(
+  data: Prisma.FundraiserOwnerCreateManyInput,
+): Promise<FundraiserOwner> {
+  return await prismaClient.fundraiserOwner.create({
+    data,
+  })
+}
+
+export async function updateFundraiserOwner(params: {
+  where: Prisma.FundraiserOwnerWhereUniqueInput
+  data: Prisma.FundraiserOwnerUpdateInput
+}): Promise<FundraiserOwner> {
+  const { data, where } = params
+  return await prismaClient.fundraiserOwner.update({
+    data,
+    where,
+  })
+}
+
+export async function deleteFundraiserOwner(
+  where: Prisma.FundraiserOwnerWhereUniqueInput,
+): Promise<FundraiserOwner> {
+  return await prismaClient.fundraiserOwner.delete({
+    where,
+  })
+}
+
+export async function deleteFundraiserOwnerByFundraiserId(
+  fundraiserId: string,
+): Promise<FundraiserOwner> {
+  return await prismaClient.fundraiserOwner.deleteMany({
+    where: {
+      fundraiserId,
+    },
+  })
+}
