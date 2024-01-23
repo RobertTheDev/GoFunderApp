dec25c6529764acde61c99b44bf05e9c1ab86e71
RobertTheDev <robertthedev@gmail.com>
2024-01-23 22:27:03 +0000

    ci: removed amazon route

--- server/src/features/auth/features/oauth/routes/oauth.routes.ts
+++ server/src/features/auth/features/oauth/routes/oauth.routes.ts
@@ -1,12 +1,10 @@
 import { type RequestHandler, Router } from 'express'
-import signInWithAmazonHandler from '../controllers/amazonOauth'
 import signInWithFacebookHandler from '../controllers/facebookOauth'
 import signInWithGithubHandler from '../controllers/githubOauth'
 import signInWithGoogleHandler from '../controllers/googleOauth'
 
 const oauthRouter = Router()
 
-oauthRouter.get('/amazon/:code', signInWithAmazonHandler as RequestHandler)
 oauthRouter.get('/facebook/:code', signInWithFacebookHandler as RequestHandler)
 oauthRouter.get('/github/:code', signInWithGithubHandler as RequestHandler)
 oauthRouter.get('/google/:code', signInWithGoogleHandler as RequestHandler)
