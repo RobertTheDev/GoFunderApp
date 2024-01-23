51436e79b91b9d7b196f1c18a610b718a23553f3
RobertTheDev <robertthedev@gmail.com>
2024-01-23 22:21:44 +0000

    feat: defined delete user handler

--- server/src/features/user/services/user.service.ts
+++ server/src/features/user/services/user.service.ts
@@ -1,41 +1,32 @@
-import { type Prisma } from '@prisma/client'
+import { type User, type Prisma } from '@prisma/client'
 import prismaClient from '../../../utils/prisma/prismaClient'
 
 export async function countUsersByUsername(username: string): Promise<number> {
   return await prismaClient.user.count({ where: { username } })
 }
 
+// Deletes a user from prisma database.
+export async function deleteUser(
+  userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+): Promise<User | null> {
+  return await prismaClient.user.delete({
+    where: userWhereUniqueInput,
+  })
+}
+
 // Returns a user from prisma database.
 export async function findUser(
   userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-): Promise<{
-  id: string
-  annonymous: boolean
-  createdAt: Date
-  defaultCurrency: string
-  name: string
-  image: string | null
-  mfaType: string | null
-  totalCharitesOwned: number
-  totalDonationsAmount: number
-  totalDonationsMade: number
-  totalFundraisersOwned: number
-} | null> {
+): Promise<User | null> {
   return await prismaClient.user.findUnique({
     where: userWhereUniqueInput,
-    select: {
-      id: true,
-      annonymous: true,
-      createdAt: true,
-      defaultCurrency: true,
-      image: true,
-      mfaType: true,
-      name: true,
-      totalCharitesOwned: true,
-      totalDonationsAmount: true,
-      totalDonationsMade: true,
-      totalFundraisersOwned: true,
-    },
+  })
+}
+
+// Returns a user from prisma database.
+export async function findUserById(id: string): Promise<User | null> {
+  return await prismaClient.user.findUnique({
+    where: { id },
   })
 }
 
@@ -46,21 +37,7 @@ export async function findUsers(params: {
   cursor?: Prisma.UserWhereUniqueInput
   where?: Prisma.UserWhereInput
   orderBy?: Prisma.UserOrderByWithRelationInput
-}): Promise<
-  Array<{
-    id: string
-    annonymous: boolean
-    createdAt: Date
-    defaultCurrency: string
-    name: string
-    image: string | null
-    mfaType: string | null
-    totalCharitesOwned: number
-    totalDonationsAmount: number
-    totalDonationsMade: number
-    totalFundraisersOwned: number
-  }>
-> {
+}): Promise<User[]> {
   const { skip, take, cursor, where, orderBy } = params
   return await prismaClient.user.findMany({
     skip,
@@ -68,18 +45,5 @@ export async function findUsers(params: {
     cursor,
     where,
     orderBy,
-    select: {
-      id: true,
-      annonymous: true,
-      createdAt: true,
-      defaultCurrency: true,
-      image: true,
-      mfaType: true,
-      name: true,
-      totalCharitesOwned: true,
-      totalDonationsAmount: true,
-      totalDonationsMade: true,
-      totalFundraisersOwned: true,
-    },
   })
 }
