492927cfc3e5c4a1d276b7d492d39131cfb2c5e7
RobertTheDev <robertthedev@gmail.com>
2024-01-24 01:40:08 +0000

    feat: created change password page

---
+++ client/src/features/auth/pages/ChangePasswordPage/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import ChangePasswordForm from "../../components/password/ChangePasswordForm";
+
+export default function ChangePasswordPage(): ReactElement {
+  return (
+    <div>
+      <ChangePasswordForm />
+    </div>
+  );
+}
