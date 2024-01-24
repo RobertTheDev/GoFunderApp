7bedbd9b5f833f841cdf24c5ed9e87f5ad30b599
RobertTheDev <robertthedev@gmail.com>
2024-01-24 01:38:10 +0000

    feat: added change password schema type

--- client/src/features/auth/service/auth.service.ts
+++ client/src/features/auth/service/auth.service.ts
@@ -3,6 +3,7 @@ import ApiResponse from "../../../interfaces/ApiResponse";
 import IUser from "../../../interfaces/User";
 import { PasswordSignUpSchemaType } from "../validators/passwordSignUp.schema";
 import { PasswordSignInSchemaType } from "../validators/passwordSignIn.schema";
+import { ChangePasswordSchemaType } from "../validators/changePassword.schema";
 
 interface UserApiResponse extends ApiResponse {
   data: IUser;
@@ -37,4 +38,16 @@ export async function getAuthenticatedUser() {
     withCredentials: true,
   });
   return user.data.data;
+}
+
+export async function changePassword(
+  data: ChangePasswordSchemaType
+): Promise<AxiosResponse<ApiResponse>> {
+  return await axios.put(
+    `${process.env.REACT_APP_API_URL}/auth/change-password`,
+    data,
+    {
+      withCredentials: true,
+    }
+  );
 }
