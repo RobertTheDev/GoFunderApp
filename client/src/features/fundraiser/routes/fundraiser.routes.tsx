e3435cd8bdb6e397d18b4d6afa9b08c89d2b6bd4
RobertTheDev <robertthedev@gmail.com>
2024-01-24 14:17:13 +0000

    feat: added delete route

--- client/src/features/fundraiser/routes/fundraiser.routes.tsx
+++ client/src/features/fundraiser/routes/fundraiser.routes.tsx
@@ -1,10 +1,14 @@
 import CreateFundraiserPage from "../pages/CreateFundraiser";
+import DeleteFundraiserPage from "../pages/DeleteFundraiser";
 import FundraiserPage from "../pages/Fundraiser";
 import FundraisersPage from "../pages/Fundraisers";
+import UpdateFundraiserPage from "../pages/UpdateFundraiser";
 
 const fundraiserRouter = [
   { path: "/fundraisers", element: <FundraisersPage /> },
   { path: "/fundraisers/:slug", element: <FundraiserPage /> },
+  { path: "/fundraisers/:slug/delete", element: <DeleteFundraiserPage /> },
+  { path: "/fundraisers/:slug/edit", element: <UpdateFundraiserPage /> },
   { path: "/start-fundraising", element: <CreateFundraiserPage /> },
 ];
 
