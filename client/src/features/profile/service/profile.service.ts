4617ed52edb474e4cf018f4d6defd689fcc54f95
RobertTheDev <robertthedev@gmail.com>
2024-01-24 01:38:26 +0000

    feat: defined delete profile handler

---
+++ client/src/features/profile/service/profile.service.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from "axios";
+import ApiResponse from "../../../interfaces/ApiResponse";
+import { DeleteProfileSchemaType } from "../validators/deleteProfile.schema";
+
+axios.defaults.withCredentials = true;
+
+const apiUrl: string = `${process.env.REACT_APP_API_URL}`;
+
+export async function deleteProfile(
+  data: DeleteProfileSchemaType
+): Promise<AxiosResponse<ApiResponse>> {
+  return await axios.post(`${apiUrl}/auth/delete`, data, {
+    withCredentials: true,
+  });
+}
