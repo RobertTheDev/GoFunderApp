d870468e16d60f8aeb81bbc1c897e1c933cb96bf
RobertTheDev <robertthedev@gmail.com>
2024-01-25 23:46:45 +0000

    feat: defined accessibility statement link

--- client/src/features/layout/components/Footer/footerLinks.ts
+++ client/src/features/layout/components/Footer/footerLinks.ts
@@ -6,6 +6,7 @@ const footerLinks: {
   { id: "1", url: "/privacy-policy", name: "Privacy Policy" },
   { id: "2", url: "/terms-and-conditions", name: "Terms and Conditions" },
   { id: "3", url: "/about", name: "About" },
+  { id: "4", url: "/accessibility-statement", name: "Accessibility Statement" },
 ];
 
 export default footerLinks;
