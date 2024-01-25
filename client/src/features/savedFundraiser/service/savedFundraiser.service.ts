6f4e145f91513aad08068dc01f89123c929b2e8b
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:03:31 +0000

    feat: defined service

---
+++ client/src/features/savedFundraiser/service/savedFundraiser.service.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from "axios";
+import ApiResponse from "../../../interfaces/ApiResponse";
+import ISavedFundraiser from "../../../interfaces/SavedFundraiser";
+import { SaveFundraiserSchemaType } from "../validators/saveFundraiser.schema";
+
+interface SavedFundraiserApiResponse extends ApiResponse {
+  data: ISavedFundraiser | null;
+}
+
+interface SavedFundraisersApiResponse extends ApiResponse {
+  data: ISavedFundraiser[];
+}
+
+const savedFundraiserApiUrl: string = `${process.env.REACT_APP_API_URL}/saved-fundraisers`;
+
+export async function getSavedFundraisers(): Promise<
+  AxiosResponse<SavedFundraisersApiResponse>
+> {
+  return await axios.get(`${savedFundraiserApiUrl}`, { withCredentials: true });
+}
+
+export async function saveFundraiser(
+  data: SaveFundraiserSchemaType
+): Promise<AxiosResponse<SavedFundraiserApiResponse>> {
+  return await axios.post(
+    `${savedFundraiserApiUrl}/save/${data.fundraiserId}`,
+    {},
+    { withCredentials: true }
+  );
+}
