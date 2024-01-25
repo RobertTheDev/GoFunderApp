8247b4c92a8c5f0407cc22a94d0901f155cc592f
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:15:42 +0000

    feat: added fundraiser id param

--- client/src/features/donation/components/CreateDonationForm/index.tsx
+++ client/src/features/donation/components/CreateDonationForm/index.tsx
@@ -1,24 +1,29 @@
 import { ReactElement } from "react";
 import useCreateDonation from "./useCreateDonation";
 
-export default function CreateDonationForm(): ReactElement {
+export default function CreateDonationForm({
+  fundraiserId,
+}: {
+  fundraiserId: string;
+}): ReactElement {
   const { errors, handleCreateDonation, message, register } =
-    useCreateDonation();
+    useCreateDonation(fundraiserId);
 
   return (
     <div>
       <form onSubmit={handleCreateDonation}>
+        <p>{fundraiserId}</p>
         <label htmlFor="amount">Amount</label>
-        <input {...register("amount")} type="number" name="amount" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          type="number"
+          name="amount"
+        />
         {errors.amount?.message && <p>{errors.amount.message}</p>}
 
         <label htmlFor="message">Message</label>
         <input {...register("message")} type="text" name="message" />
         {errors.message?.message && <p>{errors.message.message}</p>}
-
-        <label htmlFor="annonymous">Annonymous</label>
-        <input {...register("annonymous")} type="checkbox" name="annonymous" />
-        {errors.annonymous?.message && <p>{errors.annonymous.message}</p>}
 
         {message && <p>{message.content}</p>}
 
