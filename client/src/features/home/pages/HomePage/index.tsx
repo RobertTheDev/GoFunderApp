e1d4b8e1ff61cd776d26883901824d63c20114a8
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:03:48 +0000

    refactor: moved template content to page

--- client/src/features/home/pages/HomePage/index.tsx
+++ client/src/features/home/pages/HomePage/index.tsx
@@ -1,12 +1,23 @@
 import { ReactElement } from "react";
 import Seo from "../../../common/Seo";
-import HomePageTemplate from "../../templates/HomePageTemplate";
+import Banner from "../../components/Banner";
+import CategoryFundraiserSection from "../../components/CategoryFundraiserSection";
+import CategoryMenu from "../../components/CategoryMenu";
 
 export default function HomePage(): ReactElement {
   return (
     <>
       <Seo title="Home" description="" />
-      <HomePageTemplate />
+      <div>
+        <Banner />
+        <CategoryFundraiserSection category="Category" />
+        <CategoryFundraiserSection category="Disability" />
+        <CategoryFundraiserSection category="Disability" />
+        <CategoryMenu />
+        <CategoryFundraiserSection category="Disability" />
+        <CategoryFundraiserSection category={"Disability"} />
+        <CategoryFundraiserSection category="Disability" />
+      </div>
     </>
   );
 }
