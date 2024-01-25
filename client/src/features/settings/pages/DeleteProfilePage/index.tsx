8e2f1dd13ca91a498deadd2f15cad60ecb6c29f0
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:11:04 +0000

    feat: created delete profile page

---
+++ client/src/features/settings/pages/DeleteProfilePage/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import DeleteProfileForm from "../../components/DeleteProfileForm";
+
+export default function DeleteProfilePage(): ReactElement {
+  return (
+    <div>
+      <DeleteProfileForm />
+    </div>
+  );
+}
