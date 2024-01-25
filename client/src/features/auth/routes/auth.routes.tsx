9fc78f761cddfa834597359e752a5ed52d70fa28
RobertTheDev <robertthedev@gmail.com>
2024-01-25 15:58:14 +0000

    ci: removed callback routes

--- client/src/features/auth/routes/auth.routes.tsx
+++ client/src/features/auth/routes/auth.routes.tsx
@@ -1,27 +1,6 @@
-import AmazonCallbackPage from "../pages/AmazonCallback";
 import ChangePasswordPage from "../pages/ChangePasswordPage";
-import FacebookCallbackPage from "../pages/FacebookCallback";
-import GithubCallbackPage from "../pages/GithubCallback";
-import GoogleCallbackPage from "../pages/GoogleCallback";
 
 const authRouter = [
-  {
-    path: "/auth/amazon/callback",
-    element: <AmazonCallbackPage />,
-  },
-
-  {
-    path: "/auth/facebook/callback",
-    element: <FacebookCallbackPage />,
-  },
-  {
-    path: "/auth/github/callback",
-    element: <GithubCallbackPage />,
-  },
-  {
-    path: "/auth/google/callback",
-    element: <GoogleCallbackPage />,
-  },
   {
     path: "/change-password",
     element: <ChangePasswordPage />,
