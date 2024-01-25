1d72ea39027ba3ce534fc7dc7583d2b9ba5e18fb
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:16:17 +0000

    feat: fetched and mapped data

--- client/src/features/donation/pages/UserDonationsPage/index.tsx
+++ client/src/features/donation/pages/UserDonationsPage/index.tsx
@@ -1,12 +1,29 @@
 import { ReactElement } from "react";
 import Seo from "../../../common/Seo";
-import UserDonationsPageTemplate from "../../templates/UserDonationsPage";
+import { useQuery } from "@tanstack/react-query";
+import { getDonationsByCurrentUser } from "../../service/donation.service";
 
 export default function UserDonationsPage(): ReactElement {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getDonationsByCurrentUserData"],
+    queryFn: getDonationsByCurrentUser,
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: + {error.message}</p>;
+
   return (
     <>
-      <Seo title="User Donations" description="" />
-      <UserDonationsPageTemplate />
+      <Seo title="My Donations" description="" />
+
+      {data.data.data.map((donation) => {
+        return (
+          <div key={donation.id}>
+            <p>{donation.message}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
