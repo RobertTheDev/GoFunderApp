9cdc06551c3c9498ee62202f924bd4d347832dae
RobertTheDev <robertthedev@gmail.com>
2024-01-25 13:04:09 +0000

    ci: deleted file

--- client/src/features/auth/components/password/PasswordlessSignInForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-export default function PasswordlessSignInForm(): ReactElement {
-  return (
-    <div>
-      <p>PasswordlessSignInForm</p>
-    </div>
-  );
-}
