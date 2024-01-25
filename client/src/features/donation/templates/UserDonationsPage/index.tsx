db8c3dd89d533738edffdfbe043eded0e66e6d32
RobertTheDev <robertthedev@gmail.com>
2024-01-25 15:52:35 +0000

    ci: deleted file

--- client/src/features/donation/templates/UserDonationsPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-export default function UserDonationsPageTemplate(): ReactElement {
-  return (
-    <div>
-      <h1>User Donations</h1>
-    </div>
-  );
-}
--- client/src/features/notFound/templates/NotFoundPageTemplate/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-export default function NotFoundPageTemplate(): ReactElement {
-  return (
-    <div>
-      <p>Not fOUND</p>
-    </div>
-  );
-}
