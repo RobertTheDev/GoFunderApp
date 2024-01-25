304de0dc2ce246361542ab2e84d2424948ff98be
RobertTheDev <robertthedev@gmail.com>
2024-01-25 21:06:41 +0000

    feat: created settings page layout

---
+++ client/src/features/settings/layouts/SettingsPageLayout/index.tsx
@@ -0,0 +1,17 @@
+import { ReactElement, ReactNode } from "react";
+import SettingsMenu from "../../components/SettingsMenu";
+
+export default function SettingsPageLayout({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  return (
+    <div>
+      <div>
+        <SettingsMenu />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
