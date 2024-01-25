6f592e1e9a0a11a741289940982d2620c7043800
RobertTheDev <robertthedev@gmail.com>
2024-01-25 23:11:32 +0000

    feat: defined settings links

---
+++ client/src/utils/settingsLinks.ts
@@ -0,0 +1,45 @@
+const settingsLinks: {
+  name: string;
+  path: string;
+  description: string;
+  icon: string;
+}[] = [
+  {
+    name: "Change Email Address",
+    path: "/settings/change-email",
+    description: "Description",
+    icon: "",
+  },
+  {
+    name: "Change Password",
+    path: "/settings/change-password",
+    description: "Description",
+    icon: "",
+  },
+  {
+    name: "Close Account",
+    path: "/settings/close-account",
+    description: "Description",
+    icon: "",
+  },
+  {
+    name: "Send Email Verification",
+    path: "/settings/send-email-verification",
+    description: "Description",
+    icon: "",
+  },
+  {
+    name: "Setup MFA",
+    path: "/settings/setup-mfa",
+    description: "Description",
+    icon: "",
+  },
+  {
+    name: "Sessions",
+    path: "/settings/sessions",
+    description: "Description",
+    icon: "",
+  },
+];
+
+export default settingsLinks;
