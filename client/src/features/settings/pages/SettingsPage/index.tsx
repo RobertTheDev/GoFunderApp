f8b09ca080ed1d2240a943c6697ed62502d6ccaa
RobertTheDev <robertthedev@gmail.com>
2024-01-25 22:59:58 +0000

    feat: created settings page

---
+++ client/src/features/settings/pages/SettingsPage/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import Seo from "../../../common/Seo";
+
+export default function SettingsPage(): ReactElement {
+  return (
+    <>
+      <Seo title="Settings" description="" />
+      <div>
+        <p>Settings</p>
+      </div>
+    </>
+  );
+}
