6ccb80e62661e4616d1016489f48ee81cbda2a02
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:13:41 +0000

    style: added styles

--- client/src/features/fundraiser/components/CreateFundraiserForm/index.tsx
+++ client/src/features/fundraiser/components/CreateFundraiserForm/index.tsx
@@ -1,51 +1,88 @@
 import { ReactElement } from "react";
 import useCreateFundraiser from "./useCreateFundraiser";
+import styles from "./styles.module.scss";
 
 export default function CreateFundraiserForm(): ReactElement {
   const { errors, handleCreateFundraiser, message, register } =
     useCreateFundraiser();
 
   return (
-    <form onSubmit={handleCreateFundraiser}>
-      <label htmlFor="name">Name</label>
-      <input {...register("name")} type="text" name="name" />
-      {errors.name?.message && <p>{errors.name?.message}</p>}
-
-      <label htmlFor="category">Category</label>
-      <input {...register("category")} type="text" name="category" />
-      {errors.category?.message && <p>{errors.category?.message}</p>}
-
-      <label htmlFor="deadlineDate">Deadline Date</label>
-      <input
-        {...register("deadlineDate", { valueAsDate: true })}
-        type="datetime-local"
-        name="deadlineDate"
-      />
-      {errors.deadlineDate?.message && <p>{errors.deadlineDate?.message}</p>}
-
-      <label htmlFor="description">Description</label>
-      <input {...register("description")} type="text" name="description" />
-      {errors.description?.message && <p>{errors.description?.message}</p>}
-
-      <label htmlFor="headline">Headline</label>
-      <input {...register("headline")} type="text" name="headline" />
-      {errors.headline?.message && <p>{errors.headline?.message}</p>}
-
-      <label htmlFor="image">Image</label>
-      <input {...register("image")} type="url" name="image" />
-      {errors.image?.message && <p>{errors.image?.message}</p>}
-
-      <label htmlFor="target">Target</label>
-      <input
-        {...register("target", { valueAsNumber: true })}
-        type="number"
-        name="target"
-      />
-      {errors.target?.message && <p>{errors.target?.message}</p>}
-
+    <form className={styles.formContainer} onSubmit={handleCreateFundraiser}>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          {...register("name")}
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        {errors.name?.message && <p>{errors.name?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <select
+          className={styles.formInput}
+          {...register("category")}
+          name="category"
+        >
+          <option>Hello</option>
+        </select>
+        {errors.category?.message && <p>{errors.category?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          {...register("deadlineDate", { valueAsDate: true })}
+          type="datetime-local"
+          name="deadlineDate"
+          placeholder="Deadline Date"
+        />
+        {errors.deadlineDate?.message && <p>{errors.deadlineDate?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <textarea
+          className={styles.formInput}
+          {...register("description")}
+          name="description"
+          placeholder="Description"
+        />
+        {errors.description?.message && <p>{errors.description?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          {...register("headline")}
+          type="text"
+          name="headline"
+          placeholder="Headline"
+        />
+        {errors.headline?.message && <p>{errors.headline?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          {...register("image")}
+          type="url"
+          name="image"
+          placeholder="Image"
+        />
+        {errors.image?.message && <p>{errors.image?.message}</p>}
+      </div>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          {...register("target", { valueAsNumber: true })}
+          type="number"
+          name="target"
+          placeholder="Target"
+        />
+        {errors.target?.message && <p>{errors.target?.message}</p>}
+      </div>
       {message && <p>{message.content}</p>}
-
-      <button type="submit">Create Fundraiser</button>
+      <div className={styles.formSubmitButtonContainer}>
+        <button className={styles.formSubmitButton} type="submit">
+          Create Fundraiser
+        </button>
+      </div>
     </form>
   );
 }
