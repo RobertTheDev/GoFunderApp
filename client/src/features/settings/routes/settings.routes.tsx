f9b587ec1b417d41e5e0eada00079e13dfd0a21b
RobertTheDev <robertthedev@gmail.com>
2024-01-25 19:20:30 +0000

    feat: defined settings router

--- client/src/features/settings/routes/settings.routes.tsx
+++ client/src/features/settings/routes/settings.routes.tsx
@@ -1,14 +1,42 @@
+import ChangeEmailPage from "../pages/ChangeEmailPage";
+import ChangePasswordPage from "../pages/ChangePasswordPage";
 import CloseAccountPage from "../pages/CloseAccountPage";
+import SendEmailVerificationPage from "../pages/SendEmailVerificationPage";
 import SessionsPage from "../pages/SessionsPage";
+import SetUpTotpPage from "../pages/SetUpTotpPage";
+import VerifyEmailPage from "../pages/VerifyEmailPage";
 
-const settingsRouter = [
+const settingsRouter: {
+  path: string;
+  element: JSX.Element;
+}[] = [
+  {
+    path: "/settings/change-email",
+    element: <ChangeEmailPage />,
+  },
+  {
+    path: "/settings/change-password",
+    element: <ChangePasswordPage />,
+  },
+  {
+    path: "settings/close-account",
+    element: <CloseAccountPage />,
+  },
+  {
+    path: "/settings/send-email-verification",
+    element: <SendEmailVerificationPage />,
+  },
   {
     path: "/settings/sessions",
     element: <SessionsPage />,
   },
   {
-    path: "settings/close-account",
-    element: <CloseAccountPage />,
+    path: "/settings/setup-mfa",
+    element: <SetUpTotpPage />,
+  },
+  {
+    path: "/settings/verify-email",
+    element: <VerifyEmailPage />,
   },
 ];
 
