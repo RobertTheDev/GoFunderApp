0562896a5910f41e66113670b055fcc5bb8187ce
RobertTheDev <robertthedev@gmail.com>
2024-01-25 19:21:12 +0000

    feat: created component

---
+++ client/src/features/settings/components/SendEmailVerificationForm/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function SendEmailVerificationForm(): ReactElement {
+  return (
+    <div>
+      <p>Send Email Verification</p>
+    </div>
+  );
+}
