af4b801feb116d8178b2ecfc56dffa58505bf617
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:10:19 +0000

    feat: defined donations routes

--- client/src/features/donation/routes/donation.routes.tsx
+++ client/src/features/donation/routes/donation.routes.tsx
@@ -1,9 +1,5 @@
-import CharityDonationsPage from "../pages/CharityDonationsPage";
 import UserDonationsPage from "../pages/UserDonationsPage";
 
-const donationRouter = [
-  { path: "/:charityId/donations", element: <CharityDonationsPage /> },
-  { path: "/donations", element: <UserDonationsPage /> },
-];
+const donationRouter = [{ path: "/donations", element: <UserDonationsPage /> }];
 
 export default donationRouter;
