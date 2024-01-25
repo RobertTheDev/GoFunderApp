a822f2968a5d99ed451e55ced1009e1b4aa40736
RobertTheDev <robertthedev@gmail.com>
2024-01-25 21:45:15 +0000

    refactor: updated navigation path

--- client/src/features/home/components/CategoryMenu/index.tsx
+++ client/src/features/home/components/CategoryMenu/index.tsx
@@ -1,12 +1,11 @@
 import { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 
 interface ICategoryLink {
   id: string;
   image: { alt: string; url: string };
   name: string;
-  slug: string;
 }
 
 const categoryLinks: ICategoryLink[] = [
@@ -14,52 +13,51 @@ const categoryLinks: ICategoryLink[] = [
     id: "1",
     image: { alt: "", url: "" },
     name: "Animals and pets",
-    slug: "animals-and-pets",
   },
   {
     id: "2",
     image: { alt: "", url: "" },
     name: "Art and culture",
-    slug: "art-and-culture",
   },
   {
     id: "3",
     image: { alt: "", url: "" },
     name: "Education",
-    slug: "education",
   },
   {
     id: "4",
     image: { alt: "", url: "" },
     name: "International aid",
-    slug: "international-aid",
   },
   {
     id: "5",
     image: { alt: "", url: "" },
     name: "Disability",
-    slug: "disability",
   },
   {
     id: "6",
     image: { alt: "", url: "" },
     name: "Local community",
-    slug: "local-community",
   },
-  { id: "7", image: { alt: "", url: "" }, name: "Sports", slug: "sports" },
+  { id: "7", image: { alt: "", url: "" }, name: "Sports" },
   {
     id: "8",
     image: { alt: "", url: "" },
     name: "Health and medical",
-    slug: "health-and-medical",
   },
 ];
 
 function CategoryLink(categoryLink: ICategoryLink): ReactElement {
+  const navigate = useNavigate();
   return (
-    <Link to={`/categories/${categoryLink.slug}`} key={categoryLink.id}>
+    <div
+      onClick={() => {
+        navigate(`/fundraisers?category=${categoryLink.name}`);
+      }}
+      key={categoryLink.id}
+    >
       <p>{categoryLink.name}</p>
-    </Link>
+    </div>
   );
 }
 
