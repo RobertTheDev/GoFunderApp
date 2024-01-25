f4cdd488b41082d84f9c12a8ddd1cee9bd0e133c
RobertTheDev <robertthedev@gmail.com>
2024-01-25 15:59:36 +0000

    feat: defined seo variable

--- client/src/features/common/Seo/index.tsx
+++ client/src/features/common/Seo/index.tsx
@@ -2,11 +2,11 @@ import { ReactElement } from "react";
 import { Helmet } from "react-helmet-async";
 import ISeo from "../../../interfaces/Seo";
 
-export default function Seo({ description, title }: ISeo): ReactElement {
+export default function Seo(seo: ISeo): ReactElement {
   return (
     <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description}></meta>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description}></meta>
       <link rel="canonical" href="https://www.tacobell.com/" />
     </Helmet>
   );
