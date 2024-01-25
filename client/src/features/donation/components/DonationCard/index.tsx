9b00e3c8806ae8e34fbd1644fcfcbb18f5b35a98
RobertTheDev <robertthedev@gmail.com>
2024-01-25 21:44:29 +0000

    feat: added message field

--- client/src/features/donation/components/DonationCard/index.tsx
+++ client/src/features/donation/components/DonationCard/index.tsx
@@ -1,10 +1,15 @@
 import { ReactElement } from "react";
 import IDonation from "../../../../interfaces/Donation";
 
-export default function DonationCard(donation: IDonation): ReactElement {
+export default function DonationCard({
+  donation,
+}: {
+  donation: IDonation;
+}): ReactElement {
   return (
     <div>
       <p>{donation.amount}</p>
+      <p>{donation.message}</p>
       <p>{donation.id}</p>
     </div>
   );
