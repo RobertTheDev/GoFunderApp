af0341b3cadc148c27d9b892291fd5849c657392
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:00:31 +0000

    feat: defined fundraiser owner routes

---
+++ client/src/features/fundraiserOwner/routes/fundraiserOwner.routes.tsx
@@ -0,0 +1,7 @@
+import OwnedFundraisersPage from "../pages/OwnedFundraisersPage";
+
+const fundraiserOwnerRouter = [
+  { path: "/owned-fundraisers", element: <OwnedFundraisersPage /> },
+];
+
+export default fundraiserOwnerRouter;
