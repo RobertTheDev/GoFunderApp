2ee628bdd58d7f852677e54d7edd0d369e1a9bba
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:08:45 +0000

    feat: defined get donations by current user

--- client/src/features/donation/service/donation.service.ts
+++ client/src/features/donation/service/donation.service.ts
@@ -1,17 +1,20 @@
 import { CreateDonationSchemaType } from "../validators/createDonation.schema";
 import { UpdateDonationSchemaType } from "../validators/updateDonation.schema";
-import {
-  DonationApiResponse,
-  DonationsApiResponse,
-} from "../../../interfaces/Donation";
-import axios from "axios";
+import IDonation, { DonationApiResponse } from "../../../interfaces/Donation";
+import axios, { AxiosResponse } from "axios";
+import ApiResponse from "../../../interfaces/ApiResponse";
+
+interface UserDonationsApiResponse extends ApiResponse {
+  data: IDonation[];
+}
 
 const donationApiUrl: string = `${process.env.REACT_APP_API_URL}/donations`;
 
 export async function createdonation(
+  fundraiserId: string,
   data: CreateDonationSchemaType
 ): Promise<DonationApiResponse> {
-  return await axios.post(`${donationApiUrl}/create`, data);
+  return await axios.post(`${donationApiUrl}/create/${fundraiserId}`, data);
 }
 
 export async function deleteDonationById(
@@ -20,8 +23,16 @@ export async function deleteDonationById(
   return await axios.delete(`${donationApiUrl}/${donationId}`);
 }
 
-export async function getDonations(): Promise<DonationsApiResponse> {
+export async function getDonations(): Promise<UserDonationsApiResponse> {
   return await axios.get(`${donationApiUrl}`);
+}
+
+export async function getDonationsByCurrentUser(): Promise<
+  AxiosResponse<UserDonationsApiResponse>
+> {
+  return await axios.get(`${donationApiUrl}/current-user`, {
+    withCredentials: true,
+  });
 }
 
 export async function getDonationById(
