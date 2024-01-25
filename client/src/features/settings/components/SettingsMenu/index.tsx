a96b45d6e897b1de22f9146c9ff1dab7a66fcf1a
RobertTheDev <robertthedev@gmail.com>
2024-01-25 23:11:40 +0000

    feat: mapped settings links

--- client/src/features/settings/components/SettingsMenu/index.tsx
+++ client/src/features/settings/components/SettingsMenu/index.tsx
@@ -1,9 +1,18 @@
 import { ReactElement } from "react";
+import settingsLinks from "../../../../utils/settingsLinks";
+import { Link } from "react-router-dom";
 
 export default function SettingsMenu(): ReactElement {
   return (
     <div>
-      <p>Settings Menu</p>
+      <h3>Settings Menu</h3>
+      {settingsLinks.map((settingsLink) => {
+        return (
+          <Link to={settingsLink.path} key={settingsLink.path}>
+            {settingsLink.name}
+          </Link>
+        );
+      })}
     </div>
   );
 }
--- client/src/features/settings/pages/SettingsPage/index.tsx
+++ client/src/features/settings/pages/SettingsPage/index.tsx
@@ -1,12 +1,24 @@
 import { ReactElement } from "react";
 import Seo from "../../../common/Seo";
+import { Link } from "react-router-dom";
+import settingsLinks from "../../../../utils/settingsLinks";
 
 export default function SettingsPage(): ReactElement {
   return (
     <>
       <Seo title="Settings" description="" />
       <div>
-        <p>Settings</p>
+        <h1>Settings</h1>
+        {settingsLinks.map((settingsLink) => {
+          return (
+            <Link to={settingsLink.path} key={settingsLink.path}>
+              <div>
+                <p>{settingsLink.name}</p>
+                <p>{settingsLink.description}</p>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
