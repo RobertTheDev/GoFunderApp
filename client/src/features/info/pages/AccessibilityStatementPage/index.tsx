f093d30f78e4419b24b93bfbdf28e72407a2db7a
RobertTheDev <robertthedev@gmail.com>
2024-01-25 23:46:19 +0000

    feat: created accessibility statement page

---
+++ client/src/features/info/pages/AccessibilityStatementPage/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+import Seo from "../../../common/Seo";
+
+export default function AccessibilityStatementPage(): ReactElement {
+  return (
+    <div>
+      <Seo title="Accessibility Statement" description="" />
+      <p>Accessibility Statement</p>
+    </div>
+  );
+}
