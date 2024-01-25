03a80e167486580376fde9e0343673b89de83752
RobertTheDev <robertthedev@gmail.com>
2024-01-25 19:19:22 +0000

    feat: created component

---
+++ client/src/features/settings/components/ChangePasswordForm/index.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from "react";
+import useChangePassword from "./useChangePassword";
+
+export default function ChangePasswordForm(): ReactElement {
+  const { errors, handleChangePassword, message, register } =
+    useChangePassword();
+
+  return (
+    <form onSubmit={handleChangePassword}>
+      <label htmlFor="currentPassword">Current Password</label>
+      <input
+        {...register("currentPassword")}
+        type="password"
+        name="currentPassword"
+      />
+      {errors.currentPassword?.message && (
+        <p>{errors.currentPassword?.message}</p>
+      )}
+      <label htmlFor="newPassword">New Password</label>
+      <input {...register("newPassword")} type="password" name="newPassword" />
+      {errors.newPassword?.message && <p>{errors.newPassword?.message}</p>}
+
+      {message && <p>{message.content}</p>}
+
+      <button type="submit">Change Password</button>
+    </form>
+  );
+}
