038b4732e5ffe5a5f43bfcee5c93658db5badd7e
RobertTheDev <robertthedev@gmail.com>
2024-01-25 16:17:00 +0000

    feat: fetched and mapped query data

--- client/src/features/savedFundraiser/pages/SavedFundraisers/index.tsx
+++ client/src/features/savedFundraiser/pages/SavedFundraisers/index.tsx
@@ -1,11 +1,36 @@
 import { ReactElement } from "react";
 import Seo from "../../../common/Seo";
+import { useQuery } from "@tanstack/react-query";
+import { getSavedFundraisers } from "../../service/savedFundraiser.service";
+import FundraiserCard from "../../../fundraiser/components/FundraiserCard";
+import styles from "./styles.module.scss";
 
 export default function SavedFundraisersPage(): ReactElement {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getSavedFundraisersData"],
+    queryFn: getSavedFundraisers,
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: + {error.message}</p>;
+
   return (
-    <div>
+    <>
       <Seo title="Saved Fundraisers" description="" />
-      <h2>Saved Fundraisers</h2>
-    </div>
+      <div className={styles.pageContainer}>
+        <h2>Saved Fundraisers</h2>
+        <div className={styles.savedFundraiserCardsContainer}>
+          {data.data.data.map((savedFundraiser) => {
+            return (
+              <FundraiserCard
+                key={savedFundraiser.id}
+                {...savedFundraiser.fundraiser}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
