5a9e8ebc41aeafba0af6cc05ef793120b38a4458
RobertTheDev <robertthedev@gmail.com>
2024-01-27 17:13:19 +0000

    feat: added mfa verified field

--- client/src/interfaces/Session.ts
+++ client/src/interfaces/Session.ts
@@ -6,6 +6,7 @@ export default interface ISession {
   createdAt: Date;
   updatedAt: Date;
   expires: Date | null;
+  mfaVerified: Date | null;
   sessionId: string;
   user: IUser;
   userId: string;
