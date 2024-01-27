e31c99eed543a1f34b7f707814fd18f0ed803085
RobertTheDev <robertthedev@gmail.com>
2024-01-27 01:54:38 +0000

    feat: added reset password handler

--- server/src/features/auth/features/emailPassword/routes/emailPassword.routes.ts
+++ server/src/features/auth/features/emailPassword/routes/emailPassword.routes.ts
@@ -8,6 +8,8 @@ import {
   ensureUserIsAuthenticated,
   ensureUserIsNotAuthenticated,
 } from '../../../../../features/auth/middlewares/auth.middlewares.js'
+import { sendPasswordResetToken } from '../handlers/sendPasswordResetToken.js'
+import { resetPasswordWithToken } from '../handlers/resetPasswordWithToken.js'
 
 const emailPasswordRouter: Router = Router()
 
@@ -37,6 +39,18 @@ emailPasswordRouter.put(
   '/change-password',
   ensureUserIsAuthenticated as RequestHandler,
   changePassword as RequestHandler,
+)
+
+emailPasswordRouter.post(
+  '/send-password-reset',
+  ensureUserIsNotAuthenticated as RequestHandler,
+  sendPasswordResetToken as RequestHandler,
+)
+
+emailPasswordRouter.put(
+  '/reset-password/:code',
+  ensureUserIsNotAuthenticated as RequestHandler,
+  resetPasswordWithToken as RequestHandler,
 )
 
 export default emailPasswordRouter
