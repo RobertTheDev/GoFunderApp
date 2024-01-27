7f7007026a14898eefff525e0ba3086d393b6505
RobertTheDev <robertthedev@gmail.com>
2024-01-27 17:16:19 +0000

    fix: fixed api url

--- client/src/features/auth/components/totp/TotpSetupForm/index.tsx
+++ client/src/features/auth/components/totp/TotpSetupForm/index.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { ReactElement } from "react";
 import useTotpSetupForm from "./useTotpSetupForm";
+import styles from "./styles.module.scss";
 
 interface GenerateTotpSecretResponse {
   secret: string;
@@ -17,7 +18,9 @@ export default function TotpSetupForm(): ReactElement {
       refetchOnWindowFocus: false,
       queryFn: async () =>
         await axios
-          .get(`${process.env.REACT_APP_API_URL}/api/auth/totp/generate-secret`)
+          .get(`${process.env.REACT_APP_API_URL}/auth/totp/generate-secret`, {
+            withCredentials: true,
+          })
           .then((res) => res.data.data),
     });
 
@@ -31,25 +34,30 @@ export default function TotpSetupForm(): ReactElement {
   if (!data) return <p>No qr code found.</p>;
 
   return (
-    <div>
-      <h3>Set up MFA</h3>
+    <div className={styles.container}>
       <p>
-        Step 1: Search the App Store or Google Play for "TOTP Authenticator" and
-        install one of the top listed free apps. The most popular TOTP apps are
-        Authy, 2FA Authentcator, Google Authenticator and Microsoft
-        Authenticator.
+        This step is optional. To add ah extra layer of protection you can set
+        up multi-factor authentication using an authneticator app. Follow the
+        instructions below if you would like to set up MFA.
+      </p>
+      <p>
+        <strong> Step 1</strong>: Search the App Store or Google Play for "TOTP
+        Authenticator" and install one of the top listed free apps. The most
+        popular TOTP apps are Authy, 2FA Authentcator, Google Authenticator and
+        Microsoft Authenticator.
       </p>
 
       <p>
-        Step 2: Set up the app by scanning the QR code below. If the QR code
-        does not work you will need to manually enter the setup code below it.
+        <strong>Step 2</strong>: Set up the app by scanning the QR code below.
+        If the QR code does not work you will need to manually enter the setup
+        code below it.
       </p>
 
       <img src={data.qrCode} alt={data.secret} />
-      <p>{data.secret}</p>
+      <strong>{data.secret}</strong>
       <p>
-        Step 3: After scanning the QR code, the app will display a 6 digit code.
-        Enter the code below to complete MFA setup.
+        <strong>Step 3</strong>: After scanning the QR code, the app will
+        display a 6 digit code. Enter the code below to complete MFA setup.
       </p>
       <form onSubmit={handleTotpSetup}>
         <input {...register("code")} type="text" name="code" />
