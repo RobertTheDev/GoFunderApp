a06ce7994f456cb58fa708f9a9eb7b8965244fbb
RobertTheDev <robertthedev@gmail.com>
2024-01-27 17:30:50 +0000

    ci: removed email router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import authRouter from '../features/auth/routes/auth.routes.js'
 import donationRouter from '../features/donation/routes/donation.routes.js'
-import emailRouter from '../features/email/routes/email.routes.js'
 import fundraiserRouter from '../features/fundraiser/routes/fundraiser.routes.js'
 import savedFundraiserRouter from '../features/savedFundraiser/routes/savedFundraiser.routes.js'
 import uploadRouter from '../features/upload/routes/upload.routes.js'
@@ -14,7 +13,6 @@ const appRouter: Router = Router()
 // Defines app routes.
 appRouter.use('/auth', authRouter)
 appRouter.use('/donations', donationRouter)
-appRouter.use('/email', emailRouter)
 appRouter.use('/fundraisers', fundraiserRouter)
 appRouter.use('/fundraiser-owners', fundraiserOwnerRouter)
 appRouter.use('/saved-fundraisers', savedFundraiserRouter)
