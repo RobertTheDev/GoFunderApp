d27a7321f55e020db21ed2af4c1c12d4d0757d63
RobertTheDev <robertthedev@gmail.com>
2024-01-27 20:31:09 +0000

    ci: removed profile router

--- server/src/features/auth/routes/auth.routes.ts
+++ server/src/features/auth/routes/auth.routes.ts
@@ -2,13 +2,11 @@ import { Router } from 'express'
 import emailPasswordRouter from '../features/emailPassword/routes/emailPassword.routes.js'
 import totpRouter from '../features/totp/routes/totp.routes.js'
 import sessionRouter from '../features/session/routes/session.routes.js'
-import profileRouter from '../../profile/routes/profile.routes.js'
 
 // Sets up the charity router.s
 const authRouter: Router = Router()
 // Configures auth module routers.
 authRouter.use('/', emailPasswordRouter)
-authRouter.use('/profile', profileRouter)
 authRouter.use('/session', sessionRouter)
 authRouter.use('/totp', totpRouter)
 
