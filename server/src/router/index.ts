aa536ad4b706578e2f7b1d792313f3919c276926
RobertTheDev <robertthedev@gmail.com>
2024-01-27 20:31:18 +0000

    feat: added profile router

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -3,7 +3,8 @@ import authRouter from '../features/auth/routes/auth.routes.js'
 import donationRouter from '../features/donation/routes/donation.routes.js'
 import fundraiserRouter from '../features/fundraiser/routes/fundraiser.routes.js'
 import savedFundraiserRouter from '../features/savedFundraiser/routes/savedFundraiser.routes.js'
-import fundraiserOwnerRouter from 'src/features/fundraiserOwner/routes/fundraiserOwner.routes.js'
+import fundraiserOwnerRouter from '../features/fundraiserOwner/routes/fundraiserOwner.routes.js'
+import profileRouter from '../features/profile/routes/profile.routes.js'
 
 // Sets up the apps routers with the modulised routers.
 const router: Router = Router()
@@ -13,6 +14,7 @@ router.use('/auth', authRouter)
 router.use('/donations', donationRouter)
 router.use('/fundraisers', fundraiserRouter)
 router.use('/fundraiser-owners', fundraiserOwnerRouter)
+router.use('/profile', profileRouter)
 router.use('/saved-fundraisers', savedFundraiserRouter)
 
 export default router
