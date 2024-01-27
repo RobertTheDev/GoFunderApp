242e6bb20c4ff154f98d90a805c10598dc747777
RobertTheDev <robertthedev@gmail.com>
2024-01-27 19:49:16 +0000

    fix: fixed import path

--- server/src/features/auth/routes/auth.routes.ts
+++ server/src/features/auth/routes/auth.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import emailPasswordRouter from '../features/emailPassword/routes/emailPassword.routes.js'
 import totpRouter from '../features/totp/routes/totp.routes.js'
 import sessionRouter from '../features/session/routes/session.routes.js'
-import profileRouter from '../features/profile/routes/profile.routes.js'
+import profileRouter from '../../profile/routes/profile.routes.js'
 
 // Sets up the charity router.s
 const authRouter: Router = Router()
