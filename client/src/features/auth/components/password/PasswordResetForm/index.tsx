f4f7d8ada48a3321dcb15d17505b63721cd6bf02
RobertTheDev <robertthedev@gmail.com>
2024-01-27 01:54:47 +0000

    feat: added form content

--- client/src/features/auth/components/password/PasswordResetForm/index.tsx
+++ client/src/features/auth/components/password/PasswordResetForm/index.tsx
@@ -1,9 +1,36 @@
 import { ReactElement } from "react";
+import styles from "./styles.module.scss";
+import useResetPassword from "./useResetPassword";
 
 export default function PasswordResetForm(): ReactElement {
+  const { register, handleResetPassword, errors, message } = useResetPassword();
+
   return (
-    <div>
-      <p>PasswordResetForm</p>
+    <div className={styles.pageContainer}>
+      <form className={styles.formContainer} onSubmit={handleResetPassword}>
+        <div className={styles.formInputContainer}>
+          <h1>Reset Password</h1>
+          <p>Change your password using the form below.</p>
+          <input
+            className={styles.formInput}
+            {...register("password")}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          {errors.password?.message && (
+            <p className={styles.formErrorText}>{errors.password.message}</p>
+          )}
+        </div>
+
+        {message && <p>{message.content}</p>}
+
+        <div className={styles.formSubmitButtonContainer}>
+          <button className={styles.formSubmitButton} type="submit">
+            Reset Password
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
