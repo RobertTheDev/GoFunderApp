46289ae6f762b314da17898a00b5b3ec73cedd1a
RobertTheDev <robertthedev@gmail.com>
2024-01-27 17:14:22 +0000

    feat: added mfa verified check

--- server/src/features/auth/features/totp/controllers/verifyTotpCode.ts
+++ server/src/features/auth/features/totp/controllers/verifyTotpCode.ts
@@ -3,15 +3,33 @@ import twoFactor from 'node-2fa'
 import type { NextFunction, Request, Response } from 'express'
 import type ResponseBody from '../../../../../interfaces/ResponseBody'
 import { verifyTotpCodeSchema } from '../validators/verifyTotp.schema'
+import prismaClient from 'src/utils/prisma/prismaClient'
 
 export async function verifyTotpCode(
   req: Request,
   res: Response<ResponseBody>,
   next: NextFunction,
 ): Promise<void> {
-  const { body } = req
+  const { body, session } = req
+  const { user } = session
 
   try {
+    if (user == null || user === undefined) {
+      throw new Error('You must be signed in to perform this action.')
+    }
+
+    const findUser = await prismaClient.user.findUnique({
+      where: { id: user.id },
+    })
+
+    if (findUser == null) {
+      throw new Error('No user found.')
+    }
+
+    if (findUser.mfaSecret == null) {
+      throw new Error('No MFS secret is associated with the user account.')
+    }
+
     const validation = await verifyTotpCodeSchema.safeParseAsync(body)
 
     if (!validation.success) {
@@ -20,10 +38,7 @@ export async function verifyTotpCode(
 
     const { code } = validation.data
 
-    const verifyResult = twoFactor.verifyToken(
-      'QKX2KGY633TO35JUTC5Y2A25VD3XS7SX',
-      code,
-    )
+    const verifyResult = twoFactor.verifyToken(findUser.mfaSecret, code)
 
     if (verifyResult == null) {
       throw new Error(
@@ -39,10 +54,52 @@ export async function verifyTotpCode(
       throw new Error('Code has expired. Please try again with a new code.')
     }
 
+    if (user.mfaType == null) {
+      await prismaClient.user.update({
+        data: {
+          mfaType: 'authenticator',
+        },
+        where: {
+          id: user.id,
+        },
+      })
+
+      req.session.mfaVerified = new Date()
+
+      await prismaClient.session.update({
+        data: {
+          mfaVerified: new Date(),
+        },
+        where: {
+          sessionId: session.id,
+        },
+      })
+
+      res.status(StatusCodes.OK).json({
+        success: true,
+        status: ReasonPhrases.OK,
+        message: 'Successfully set up MFA.',
+        data: null,
+      })
+
+      return
+    }
+
+    await prismaClient.session.update({
+      data: {
+        mfaVerified: new Date(),
+      },
+      where: {
+        sessionId: session.id,
+      },
+    })
+
+    req.session.mfaVerified = new Date()
+
     res.status(StatusCodes.OK).json({
       success: true,
       status: ReasonPhrases.OK,
-      message: 'Successfully verfified code.',
+      message: 'Successfully verified MFA.',
       data: null,
     })
   } catch (error) {
