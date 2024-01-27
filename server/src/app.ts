d1f99232b91f360a4cbc617613544f5f6a412c2f
RobertTheDev <robertthedev@gmail.com>
2024-01-27 23:03:04 +0000

    feat: added configs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,27 @@
+import { json } from 'express'
+import app from './utils/app'
 import startServer from './utils/server'
+import router from './router'
+import corsConfig from './utils/cors/corsConfig'
+import errorHandler from './middlewares/errorHandler/errorHandler.middleware'
+import sessionConfig from './utils/session/sessionOptions'
+import session from 'express-session'
+import compression from 'compression'
+import helmet from 'helmet'
+
+app.use(json())
+
+app.use(corsConfig)
+
+app.use(compression())
+
+app.use(helmet())
+
+app.use('/api', router)
+
+app.use(session(sessionConfig))
+
+app.use(errorHandler)
 
 // Runs the express server.
 startServer()
