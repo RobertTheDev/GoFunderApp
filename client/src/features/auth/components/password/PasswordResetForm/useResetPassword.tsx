5e26073c4c9f965f9ab1b059dc36237b311c3382
RobertTheDev <robertthedev@gmail.com>
2024-01-27 01:53:11 +0000

    feat: created schema

---
+++ client/src/features/auth/components/password/PasswordResetForm/useResetPassword.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { resetPassword } from "../../../service/auth.service";
+
+import resetPasswordSchema, {
+  ResetPasswordSchemaType,
+} from "../../../validators/resetPassword.schema";
+import { useSearchParams } from "react-router-dom";
+
+const useResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ResetPasswordSchemaType>({
+    resolver: zodResolver(resetPasswordSchema),
+  });
+
+  const [searchParams] = useSearchParams();
+
+  const code = searchParams.get("code");
+
+  const [message, setMessage] = useState<{
+    type: string;
+    content: string;
+  } | null>(null);
+
+  const handleResetPasswor = async (data: ResetPasswordSchemaType) => {
+    try {
+      if (code == null || code === undefined) {
+        setMessage({
+          type: "error",
+          content: "No verification code was provded.",
+        });
+
+        return;
+      }
+      const resetPassworde = await resetPassword(code, data);
+
+      setMessage({ type: "success", content: resetPassworde.data.message });
+
+      return resetPassworde;
+    } catch (error: any) {
+      setMessage({ type: "error", content: error.response.data.message });
+    }
+  };
+
+  const handleResetPassword = handleSubmit(handleResetPasswor);
+
+  return {
+    errors,
+    handleResetPassword,
+    message,
+    register,
+  };
+};
+
+export default useResetPassword;
