8909c8f0359ada7886c63875ccdf69232e914a83
RobertTheDev <robertthedev@gmail.com>
2024-01-28 01:39:58 +0000

    refactor: updated fields

--- server/src/interfaces/Profile.ts
+++ server/src/interfaces/Profile.ts
@@ -1,23 +1,13 @@
-import type ICharityOwner from './CharityOwner'
-import type IDonation from './Donation'
-import type ISavedFundraiser from './SavedFundraiser'
-
 export default interface IProfile {
-  id: string
-  createdAt: Date
-  updatedAt: Date
   annonymous: boolean
-  charitiesOwned: ICharityOwner[]
+  createdAt: Date
   defaultCurrency: string
-  donations: IDonation[]
   email: string | null
   emailVerified: Date | null
   image: string | null
-  mfaSecret: string | null
+  id: string
   mfaType: string | null
   name: string
-  password: string | null
-  savedFundraisers: ISavedFundraiser[]
   totalCharitesOwned: number
   totalDonationsAmount: number
   totalDonationsMade: number
