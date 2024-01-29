fb52035e9b1a71f1e08cbc81cff433b488484964
RobertTheDev <robertthedev@gmail.com>
2024-01-29 20:15:21 +0000

    feat: defined settings paths

---
+++ client/src/features/settings/routes/settingsPaths.ts
@@ -0,0 +1,12 @@
+const settingsPaths = {
+  changeEmail: "/settings/change-email",
+  changePassword: "/settings/change-password",
+  closeAccount: "/settings/close-account",
+  sendEmailVerification: "/settings/send-email-verification",
+  sessions: "/settings/sessions",
+  settings: "/settings",
+  setupMfa: "/settings/setup-mfa",
+  verifyEmail: "/settings/verify-email",
+};
+
+export default settingsPaths;
