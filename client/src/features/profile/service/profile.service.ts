48443447e66029070f9263ea523b46a181ba34fa
RobertTheDev <robertthedev@gmail.com>
2024-01-29 18:23:16 +0000

    feat: created service

---
+++ client/src/features/profile/service/profile.service.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosResponse } from "axios";
+import ApiResponse from "../../../interfaces/ApiResponse.js";
+import { EditProfileSchemaType } from "../validators/editProfile.schema.js";
+import IProfile from "../../../interfaces/Profile.js";
+
+interface ProfileApiResponse extends ApiResponse {
+  data: IProfile | null;
+}
+
+const profileApiUrl: string = `${process.env.REACT_APP_API_URL}/profile`;
+
+export async function updateProfile(
+  data: EditProfileSchemaType
+): Promise<AxiosResponse<ProfileApiResponse>> {
+  return await axios.put(`${profileApiUrl}/update-profile`, data, {
+    withCredentials: true,
+  });
+}
