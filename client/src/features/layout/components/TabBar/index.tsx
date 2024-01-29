2efc2efafea9ab3974cd2e302f78b1beaf4d4a81
RobertTheDev <robertthedev@gmail.com>
2024-01-29 18:21:17 +0000

    feat: created tab bar

--- client/src/features/layout/components/TabBar/index.tsx
+++ client/src/features/layout/components/TabBar/index.tsx
@@ -1,16 +1,32 @@
 import { ReactElement } from "react";
 import styles from "./styles.module.scss";
 import tabBarLinks from "./tabBarLinks";
+import { useLocation, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function TabBar(): ReactElement {
+  const navigate = useNavigate();
+
+  const { pathname } = useLocation();
+
   return (
     <div className={styles.tabBarContainer}>
       {tabBarLinks.map((tabBarLink) => {
         return (
-          <button key={tabBarLink.name} className={styles.tabBarButton}>
+          <motion.button
+            initial={{ scale: 1 }}
+            animate={{ scale: 0.9 }}
+            key={tabBarLink.name}
+            onClick={() => navigate(tabBarLink.path)}
+            className={
+              tabBarLink.path === pathname
+                ? styles.tabBarButtonActive
+                : styles.tabBarButton
+            }
+          >
             {tabBarLink.icon}
             <p>{tabBarLink.name}</p>
-          </button>
+          </motion.button>
         );
       })}
     </div>
