2875a71e580c68f8d926856311d366983db2a5fa
RobertTheDev <robertthedev@gmail.com>
2024-01-29 21:45:28 +0000

    feat: created page

---
+++ client/src/features/error/pages/UnauthorizedPage/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import Seo from "../../../common/Seo";
+
+export default function UnauthorizedPage(): ReactElement {
+  return (
+    <>
+      <Seo title="Unauthorized" description="" />
+      <div>
+        <p>Unauthorized.</p>
+      </div>
+    </>
+  );
+}
