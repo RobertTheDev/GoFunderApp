754d2ebb49e2d50151ffe56e105d8e3352c2adf7
RobertTheDev <robertthedev@gmail.com>
2024-01-29 18:19:02 +0000

    feat: added name and path fields

--- client/src/utils/categoryOptions.ts
+++ client/src/utils/categoryOptions.ts
@@ -1,13 +1,44 @@
 // These are the available allowed category options for a charity.
-const categoryOptions: string[] = [
-  "Animals and pets",
-  "Art and culture",
-  "Disability",
-  "Education",
-  "Health and medical",
-  "International aid",
-  "Local community",
-  "Sports",
+const categoryOptions: { name: string; path: string }[] = [
+  {
+    name: "Animals and Pets",
+    path: "animals-and-pets",
+  },
+
+  {
+    name: "Art and culture",
+    path: "art-and-culture",
+  },
+
+  {
+    name: "Disability",
+    path: "disability",
+  },
+
+  {
+    name: "Education",
+    path: "education",
+  },
+
+  {
+    name: "Health and medical",
+    path: "health-and-medical",
+  },
+
+  {
+    name: "International aid",
+    path: "international-aid",
+  },
+
+  {
+    name: "Local community",
+    path: "local-community",
+  },
+
+  {
+    name: "Sports",
+    path: "sports",
+  },
 ];
 
 export default categoryOptions;
