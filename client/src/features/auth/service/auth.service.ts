a2bfd6605965395365d801e0941523de8caae563
RobertTheDev <robertthedev@gmail.com>
2024-01-29 19:13:09 +0000

    feat: defined auth api routes

--- client/src/features/auth/service/auth.service.ts
+++ client/src/features/auth/service/auth.service.ts
@@ -11,37 +11,23 @@ interface UserApiResponse extends ApiResponse {
   data: IUser;
 }
 
-const authUrl: string = `${process.env.REACT_APP_API_URL}/auth`;
+const apiUrl: string = String(process.env.REACT_APP_API_URL);
 
-export async function resetPassword(
-  code: string,
-  data: ResetPasswordSchemaType
-): Promise<AxiosResponse<UserApiResponse>> {
-  return await axios.put(`${authUrl}/reset-password/${code}`, data, {
-    withCredentials: true,
-  });
-}
+const authUrl: string = `${apiUrl}/auth`;
 
-export async function sendPasswordReset(
-  data: SendPasswordResetSchemaType
-): Promise<AxiosResponse<UserApiResponse>> {
-  return await axios.post(`${authUrl}/send-password-reset`, data, {
-    withCredentials: true,
-  });
-}
+const authApiRoutes = {
+  changePassword: `${authUrl}/change-password`,
+  getAuthenticatedUser: `${apiUrl}/profile`,
+  resetPassword: (code: string) => `${authUrl}/send-password-reset/${code}`,
+  sendPasswordReset: `${authUrl}/send-password-reset`,
+  signInWithPassword: `${authUrl}/password/sign-in`,
+  signUpWithPassword: `${authUrl}/password/sign-up`,
+};
 
-export async function signUpWithPassword(
-  data: PasswordSignUpSchemaType
-): Promise<AxiosResponse<UserApiResponse>> {
-  return await axios.post(`${authUrl}/password/sign-up`, data, {
-    withCredentials: true,
-  });
-}
-
-export async function signInWithPassword(
-  data: PasswordSignInSchemaType
-): Promise<AxiosResponse<UserApiResponse>> {
-  return await axios.post(`${authUrl}/password/sign-in`, data, {
+export async function changePassword(
+  data: ChangePasswordSchemaType
+): Promise<AxiosResponse<ApiResponse>> {
+  return await axios.put(authApiRoutes.changePassword, data, {
     withCredentials: true,
   });
 }
@@ -49,15 +35,40 @@ export async function signInWithPassword(
 export async function getAuthenticatedUser(): Promise<
   AxiosResponse<UserApiResponse>
 > {
-  return await axios.get(`http://localhost:3001/api/profile`, {
+  return await axios.get(authApiRoutes.getAuthenticatedUser, {
     withCredentials: true,
   });
 }
 
-export async function changePassword(
-  data: ChangePasswordSchemaType
-): Promise<AxiosResponse<ApiResponse>> {
-  return await axios.put(`${authUrl}/change-password`, data, {
+export async function resetPassword(
+  code: string,
+  data: ResetPasswordSchemaType
+): Promise<AxiosResponse<UserApiResponse>> {
+  return await axios.put(`${authApiRoutes.resetPassword(code)}`, data, {
+    withCredentials: true,
+  });
+}
+
+export async function sendPasswordReset(
+  data: SendPasswordResetSchemaType
+): Promise<AxiosResponse<UserApiResponse>> {
+  return await axios.post(authApiRoutes.sendPasswordReset, data, {
+    withCredentials: true,
+  });
+}
+
+export async function signInWithPassword(
+  data: PasswordSignInSchemaType
+): Promise<AxiosResponse<UserApiResponse>> {
+  return await axios.post(authApiRoutes.signInWithPassword, data, {
+    withCredentials: true,
+  });
+}
+
+export async function signUpWithPassword(
+  data: PasswordSignUpSchemaType
+): Promise<AxiosResponse<UserApiResponse>> {
+  return await axios.post(authApiRoutes.signUpWithPassword, data, {
     withCredentials: true,
   });
 }
