dadc69d7e3b72f15899bbf31b1fd4630c4d90ec6
RobertTheDev <robertthedev@gmail.com>
2024-01-29 21:19:55 +0000

    feat: created owned fundraiser card

---
+++ client/src/features/fundraiserOwner/components/OwnedFundraiserCard/index.tsx
@@ -0,0 +1,42 @@
+import { ReactElement } from "react";
+import IFundraiser from "../../../../interfaces/Fundraiser";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
+
+export default function OwnedFundraiserCard({
+  fundraiser,
+}: {
+  fundraiser: IFundraiser;
+}): ReactElement {
+  return (
+    <Link
+      className={styles.cardContainer}
+      to={`/fundraisers/${fundraiser.slug}/admin`}
+    >
+      <div className={styles.cardImageContainer}>
+        <img className={styles.cardImage} src={fundraiser.imageUrl} alt="ddd" />
+      </div>
+      <div className={styles.cardContentContainer}>
+        <p className={styles.cardTitleText}>{fundraiser.name}</p>
+
+        <p>
+          £{fundraiser.totalRaised.toLocaleString()} raised of £
+          {fundraiser.target.toLocaleString()} target
+        </p>
+
+        <div className={styles.cardProgressBarContainer}>
+          <svg
+            className={styles.cardProgressBar}
+            height={"100%"}
+            width={`${(fundraiser.totalRaised / fundraiser.target) * 100}%`}
+          />
+        </div>
+
+        <p>
+          {fundraiser.totalDonations}{" "}
+          {fundraiser.totalDonations === 1 ? "donation" : "donations"}
+        </p>
+      </div>
+    </Link>
+  );
+}
