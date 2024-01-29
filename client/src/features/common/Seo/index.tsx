2689c12660d9f06e002c92d65a79b763493b29b4
RobertTheDev <robertthedev@gmail.com>
2024-01-29 20:16:11 +0000

    refactor: updated seo title

--- client/src/features/common/Seo/index.tsx
+++ client/src/features/common/Seo/index.tsx
@@ -5,7 +5,7 @@ import ISeo from "../../../interfaces/Seo";
 export default function Seo(seo: ISeo): ReactElement {
   return (
     <Helmet>
-      <title>{seo.title}</title>
+      <title>{`${seo.title} | GoFunder`}</title>
       <meta name="description" content={seo.description}></meta>
       <link rel="canonical" href="https://www.tacobell.com/" />
     </Helmet>
