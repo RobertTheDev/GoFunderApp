1c09a74e9339ad89f556f67d826b34da0c270fef
RobertTheDev <robertthedev@gmail.com>
2024-01-30 09:34:05 +0000

    feat: defined header links

---
+++ client/src/utils/headerLinks.ts
@@ -0,0 +1,16 @@
+const headerLinks = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Find Fundraisers',
+    path: '/fundraisers',
+  },
+  {
+    name: 'Start Fundraising',
+    path: '/start-fundraising',
+  },
+]
+
+export default headerLinks
