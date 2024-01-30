3db85b6bfba2be79900fcaeca14e405ef4d13565
RobertTheDev <robertthedev@gmail.com>
2024-01-30 09:33:57 +0000

    feat: defined profile menu links

---
+++ client/src/utils/profileMenuLinks.ts
@@ -0,0 +1,24 @@
+const profileMenuLinks = [
+  {
+    name: 'Edit Profile',
+    path: '/edit-profile',
+  },
+  {
+    name: 'Settings',
+    path: '/settings',
+  },
+  {
+    name: 'Saved Fundraisers',
+    path: '/saved-fundraisers',
+  },
+  {
+    name: 'My Donations',
+    path: '/donations',
+  },
+  {
+    name: 'My Fundraisers',
+    path: '/owned-fundraisers',
+  },
+]
+
+export default profileMenuLinks
