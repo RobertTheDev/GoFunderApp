284dcd5c877d26f9ea6768c8939cc951429fcea5
RobertTheDev <robertthedev@gmail.com>
2024-01-30 04:19:31 +0000

    feat: created check fundraiser owner handler

--- client/src/features/fundraiserOwner/service/fundraiserOwner.service.ts
+++ client/src/features/fundraiserOwner/service/fundraiserOwner.service.ts
@@ -6,10 +6,22 @@ interface FundraiserOwnersApiResponse extends ApiResponse {
   data: IFundraiserOwner[];
 }
 
+interface FundraiserOwnerApiResponse extends ApiResponse {
+  data: IFundraiserOwner | null;
+}
+
 const fundraiserApiUrl: string = `${process.env.REACT_APP_API_URL}/fundraiser-owner`;
 
 export async function getFundraiserOwnersByAuthenticatedUser(): Promise<
   AxiosResponse<FundraiserOwnersApiResponse>
 > {
   return await axios.get(`${fundraiserApiUrl}/user`, { withCredentials: true });
+}
+
+export async function checkFundraiserOwnerByFundraiserSlug(
+  slug: string
+): Promise<AxiosResponse<FundraiserOwnerApiResponse>> {
+  return await axios.get(`${fundraiserApiUrl}/check-fundraiser-owner/${slug}`, {
+    withCredentials: true,
+  });
 }
