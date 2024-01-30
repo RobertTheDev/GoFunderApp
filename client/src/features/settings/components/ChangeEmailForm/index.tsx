4db5bfe1f971880d76408b6baaf313a80a5cc061
RobertTheDev <robertthedev@gmail.com>
2024-01-30 21:55:01 +0000

    feat: added form hook

--- client/src/features/settings/components/ChangeEmailForm/index.tsx
+++ client/src/features/settings/components/ChangeEmailForm/index.tsx
@@ -1,9 +1,21 @@
 import { ReactElement } from 'react'
+import useChangeEmail from './useChangeEmail'
 
 export default function ChangeEmailForm(): ReactElement {
+  const { errors, handleChangeEmail, message, register } = useChangeEmail()
+
   return (
-    <div>
-      <p>Change Email</p>
-    </div>
+    <form onSubmit={handleChangeEmail}>
+      <label htmlFor='email'>Email</label>
+      <input {...register('email')} type='email' name='email' />
+      {errors.email?.message && <p>{errors.email?.message}</p>}
+      <label htmlFor='password'>Password</label>
+      <input {...register('password')} type='password' name='password' />
+      {errors.password?.message && <p>{errors.password?.message}</p>}
+
+      {message && <p>{message.content}</p>}
+
+      <button type='submit'>Change Email</button>
+    </form>
   )
 }
--- client/src/features/settings/components/SendEmailVerificationForm/index.tsx
+++ client/src/features/settings/components/SendEmailVerificationForm/index.tsx
@@ -1,9 +1,19 @@
 import { ReactElement } from 'react'
+import useSendEmailVerificationForm from './useSendEmailVerificationForm'
 
 export default function SendEmailVerificationForm(): ReactElement {
+  const { errors, handleSendEmailVerification, message, register } =
+    useSendEmailVerificationForm()
+
   return (
-    <div>
-      <p>Send Email Verification</p>
-    </div>
+    <form onSubmit={handleSendEmailVerification}>
+      <label htmlFor='email'>Email</label>
+      <input {...register('email')} type='email' name='email' />
+      {errors.email?.message && <p>{errors.email?.message}</p>}
+
+      {message && <p>{message.content}</p>}
+
+      <button type='submit'>Send Email Verification</button>
+    </form>
   )
 }
