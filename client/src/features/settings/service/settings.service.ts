554e971e457abba98ae7e207a9c8d35717756440
RobertTheDev <robertthedev@gmail.com>
2024-01-30 21:54:45 +0000

    feat: define handlers

--- client/src/features/settings/service/settings.service.ts
+++ client/src/features/settings/service/settings.service.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosResponse } from 'axios'
 import ApiResponse from '../../../interfaces/ApiResponse'
 import { DeleteProfileSchemaType } from '../validators/deleteProfile.schema'
+import { ChangeEmailSchemaType } from '../validators/changeEmail.schema'
+import { SendEmailVerificationSchemaType } from '../validators/sendEmailVerification.schema'
 
 const apiUrl = `${process.env.REACT_APP_API_URL}`
 
@@ -10,4 +12,24 @@ export async function deleteProfile(
   return await axios.post(`${apiUrl}/auth/delete`, data, {
     withCredentials: true,
   })
+}
+
+export async function changeEmail(
+  data: ChangeEmailSchemaType,
+): Promise<AxiosResponse<ApiResponse>> {
+  return await axios.put(`${apiUrl}/auth/change-email`, data, {
+    withCredentials: true,
+  })
+}
+
+export async function sendEmailVerification(
+  data: SendEmailVerificationSchemaType,
+): Promise<AxiosResponse<ApiResponse>> {
+  return await axios.post(
+    `${apiUrl}/auth/password/send-email-verification`,
+    data,
+    {
+      withCredentials: true,
+    },
+  )
 }
