fd3b7a6063a14a10085b0afe4f73d46b56e9f5b3
RobertTheDev <robertthedev@gmail.com>
2024-01-30 04:20:00 +0000

    feat: added authorized route

--- client/src/features/fundraiserAdmin/routes/fundraiserAdmin.routes.tsx
+++ client/src/features/fundraiserAdmin/routes/fundraiserAdmin.routes.tsx
@@ -3,23 +3,40 @@ import CloseFundraiserPage from "../pages/CloseFundraiserPage";
 import FundraiserAdminPage from "../pages/FundraiserAdminPage";
 import FundraiserDonationsPage from "../pages/FundraiserDonationsPage";
 import UpdateFundraiserPage from "../pages/UpdateFundraiserPage";
+import AuthorizedFundraiserOwnerRoute from "./AuthorizedFundraiserOwnerRoute";
 
 const fundraiserAdminRouter: IRouterRoute[] = [
   {
     path: "/fundraisers/:fundraiserId/admin/close-fundraiser",
-    element: <CloseFundraiserPage />,
+    element: (
+      <AuthorizedFundraiserOwnerRoute>
+        <CloseFundraiserPage />
+      </AuthorizedFundraiserOwnerRoute>
+    ),
   },
   {
     path: "/fundraisers/:fundraiserId/admin",
-    element: <FundraiserAdminPage />,
+    element: (
+      <AuthorizedFundraiserOwnerRoute>
+        <FundraiserAdminPage />
+      </AuthorizedFundraiserOwnerRoute>
+    ),
   },
   {
     path: "/fundraisers/:fundraiserId/admin/donations",
-    element: <FundraiserDonationsPage />,
+    element: (
+      <AuthorizedFundraiserOwnerRoute>
+        <FundraiserDonationsPage />
+      </AuthorizedFundraiserOwnerRoute>
+    ),
   },
   {
     path: "/fundraisers/:fundraiserId/admin/update",
-    element: <UpdateFundraiserPage />,
+    element: (
+      <AuthorizedFundraiserOwnerRoute>
+        <UpdateFundraiserPage />
+      </AuthorizedFundraiserOwnerRoute>
+    ),
   },
 ];
 
