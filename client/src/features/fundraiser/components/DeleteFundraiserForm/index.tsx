1844413e3797d189d283e2cd9d6e14a16bdb8d87
RobertTheDev <robertthedev@gmail.com>
2024-01-30 09:09:27 +0000

    ci: deleted file

--- client/src/features/fundraiser/components/DeleteFundraiserForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react'
-
-export default function DeleteFundraiserForm(): ReactElement {
-  return (
-    <div>
-      <p>Delete Fundraiser Form</p>
-    </div>
-  )
-}
--- client/src/features/fundraiserAdmin/components/UpdateFundraiserForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react'
-
-export default function UpdateFundraiserForm(): ReactElement {
-  return (
-    <div>
-      <p>Update Fundraiser Form</p>
-    </div>
-  )
-}
