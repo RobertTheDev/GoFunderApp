014569ae899f08b689cc95460e7439897ae2ec59
RobertTheDev <robertthedev@gmail.com>
2024-01-30 02:31:04 +0000

    ci: removed redundant routes

--- client/src/features/fundraiser/routes/fundraiser.routes.tsx
+++ client/src/features/fundraiser/routes/fundraiser.routes.tsx
@@ -1,15 +1,11 @@
 import IRouterRoute from "../../../interfaces/RouterRoute";
 import CreateFundraiserPage from "../pages/CreateFundraiserPage";
-import DeleteFundraiserPage from "../pages/DeleteFundraiserPage";
 import FundraiserPage from "../pages/FundraiserPage";
 import FundraisersPage from "../pages/FundraisersPage";
-import UpdateFundraiserPage from "../pages/UpdateFundraiserPage";
 
 const fundraiserRouter: IRouterRoute[] = [
   { path: "/fundraisers", element: <FundraisersPage /> },
   { path: "/fundraisers/:slug", element: <FundraiserPage /> },
-  { path: "/fundraisers/:slug/delete", element: <DeleteFundraiserPage /> },
-  { path: "/fundraisers/:slug/edit", element: <UpdateFundraiserPage /> },
   { path: "/start-fundraising", element: <CreateFundraiserPage /> },
 ];
 
