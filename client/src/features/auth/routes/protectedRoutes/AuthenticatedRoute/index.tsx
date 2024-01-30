5671abef3232700ce92c795c8deaa11d0592647a
RobertTheDev <robertthedev@gmail.com>
2024-01-30 02:30:39 +0000

    feat: created authenticated route

---
+++ client/src/features/auth/routes/protectedRoutes/AuthenticatedRoute/index.tsx
@@ -0,0 +1,25 @@
+import { Navigate } from "react-router-dom";
+import { ReactElement, ReactNode } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getAuthenticatedUser } from "../../../service/auth.service";
+
+function AuthenticatedRoute({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getProfileData"],
+    queryFn: getAuthenticatedUser,
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (data === null || data?.data.data === null || error) {
+    return <Navigate to={"/unauthenticated"} />;
+  }
+
+  return <>{children}</>;
+}
+
+export default AuthenticatedRoute;
