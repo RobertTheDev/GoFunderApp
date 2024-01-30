11c84b68c9c250320b7cdb214f0b34e3ed67e801
RobertTheDev <robertthedev@gmail.com>
2024-01-30 09:11:14 +0000

    feat: defined fundraiser admin links

---
+++ client/src/utils/fundraiserAdminLinks.ts
@@ -0,0 +1,23 @@
+const fundraiserAdminLinks: {
+  name: string
+  path: string
+  description: string
+}[] = [
+  {
+    name: 'Donations',
+    path: 'donations',
+    description: '',
+  },
+  {
+    name: 'Update Fundraiser',
+    path: 'update',
+    description: '',
+  },
+  {
+    name: 'Close Fundraiser',
+    path: 'close-fundraiser',
+    description: '',
+  },
+]
+
+export default fundraiserAdminLinks
