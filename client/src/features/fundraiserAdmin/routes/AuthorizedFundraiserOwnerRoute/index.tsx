b792aa4e7a42a248fcf6940d28cf30d5d2f56863
RobertTheDev <robertthedev@gmail.com>
2024-01-30 04:19:54 +0000

    feat: created authorized route

---
+++ client/src/features/fundraiserAdmin/routes/AuthorizedFundraiserOwnerRoute/index.tsx
@@ -0,0 +1,27 @@
+import { Navigate, useParams } from "react-router-dom";
+import { ReactElement, ReactNode } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { checkFundraiserOwnerByFundraiserSlug } from "../../../fundraiserOwner/service/fundraiserOwner.service";
+
+function AuthorizedFundraiserOwnerRoute({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  const { fundraiserId } = useParams();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["checkFundraiserOwnerData"],
+    queryFn: () => checkFundraiserOwnerByFundraiserSlug(String(fundraiserId)),
+  });
+
+  if (isPending) return <p>Loading...</p>;
+
+  if (data === null || data?.data.data === null || error) {
+    return <Navigate to={"/unauthorized"} />;
+  }
+
+  return <>{children}</>;
+}
+
+export default AuthorizedFundraiserOwnerRoute;
